8420285eceb90df9fd67003606f51ce46629d666
GrayJack <gr41.j4ck@gmail.com>
2019-09-12 12:34:11 -0300

    Core: Types: Add some handy functions

--- coreutils_core/src/types.rs
+++ coreutils_core/src/types.rs
@@ -1,12 +1,32 @@
-use libc::{gid_t, time_t, uid_t};
 #[cfg(any(target_os = "freebsd", target_os = "dragonflybsd"))]
 use libc::c_int;
+use libc::{getegid, geteuid, getgid, getuid, gid_t, time_t, uid_t};
 
 /// Group ID type.
 pub type Gid = gid_t;
 
+/// Get the current running process user effective group id.
+pub fn get_effective_gid() -> Uid {
+    unsafe { getegid() }
+}
+
+/// Get the current running process user real group id.
+pub fn get_real_gid() -> Uid {
+    unsafe { getgid() }
+}
+
 /// User ID type.
 pub type Uid = uid_t;
+
+/// Get the current running process user effective user id.
+pub fn get_effective_uid() -> Uid {
+    unsafe { geteuid() }
+}
+
+/// Get the current running process user real user id.
+pub fn get_real_uid() -> Uid {
+    unsafe { getuid() }
+}
 
 /// `Passwd` time type
 pub type Time = time_t;
