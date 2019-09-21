6242fd28f2e344e2c6d13283daf3591de4447ed6
GrayJack <gr41.j4ck@gmail.com>
2019-09-21 06:47:02 -0300

    Core: Audit: better error return on auditid function

--- coreutils_core/src/audit.rs
+++ coreutils_core/src/audit.rs
@@ -1,7 +1,21 @@
 #![allow(non_camel_case_types)]
-use std::mem::MaybeUninit;
+use std::{
+    fmt::{self, Display},
+    mem::MaybeUninit,
+};
 
 use libc::{c_int, c_uint, dev_t, pid_t, uid_t};
+
+#[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Hash)]
+pub struct AuditError {
+    err: String,
+}
+
+impl Display for AuditError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.err)
+    }
+}
 
 pub type au_id_t = uid_t;
 pub type au_asid_t = pid_t;
@@ -9,6 +23,7 @@ pub type au_event_t = c_uint;
 pub type au_emod_t = c_uint;
 pub type au_class_t = c_int;
 
+#[derive(Debug)]
 #[repr(C)]
 pub struct au_mask {
     pub am_success: c_uint,
@@ -16,19 +31,21 @@ pub struct au_mask {
 }
 pub type au_mask_t = au_mask;
 
+#[derive(Debug)]
 #[repr(C)]
 pub struct au_tid_addr {
     pub port: dev_t,
 }
 pub type au_tid_addr_t = au_tid_addr;
 
+#[derive(Debug)]
 #[repr(C)]
 pub struct c_auditinfo_addr {
     pub ai_auid: au_id_t,         // Audit user ID
     pub ai_mask: au_mask_t,       // Audit masks.
     pub ai_termid: au_tid_addr_t, // Terminal ID.
     pub ai_asid: au_asid_t,       // Audit session ID.
-    pub ai_flags: u64,       // Audit session flags
+    pub ai_flags: u64,            // Audit session flags
 }
 pub type c_auditinfo_addr_t = c_auditinfo_addr;
 
@@ -36,12 +53,14 @@ extern "C" {
     pub fn getaudit(auditinfo_addr: *mut c_auditinfo_addr_t) -> c_int;
 }
 
-pub fn auditid() {
+pub fn auditid() -> Result<(), AuditError> {
     let mut auditinfo: MaybeUninit<c_auditinfo_addr_t> = MaybeUninit::zeroed();
     let address = auditinfo.as_mut_ptr() as *mut c_auditinfo_addr_t;
+
     if unsafe { getaudit(address) } < 0 {
-        println!("couldn't retrieve information");
-        return;
+        return Err(AuditError {
+            err: "Couldn't retrieve information".to_string(),
+        });
     }
 
     let auditinfo = unsafe { auditinfo.assume_init() };
@@ -51,4 +70,5 @@ pub fn auditid() {
     println!("mask.failure=0x{:x}", auditinfo.ai_mask.am_failure);
     println!("termid.port=0x{:x}", auditinfo.ai_termid.port);
     println!("asid={}", auditinfo.ai_asid);
+    Ok(())
 }
