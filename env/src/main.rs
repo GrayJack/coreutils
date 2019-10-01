58b102a435d58baf0fe25629f41bab914811c37b
Federico Ponzi <isaacisback92@gmail.com>
2019-10-02 00:01:10 +0200

    Adds env command

--- env/src/main.rs
+++ env/src/main.rs
@@ -1,30 +1,39 @@
-use std::{
-    io::{self, Write},
-    iter::Peekable,
-    process,
-    str::Chars,
-};
+use std::collections::HashMap;
+use std::process;
+use std::process::Command;
+use std::{env, io};
 
 use clap::{load_yaml, App};
 
 fn main() {
     let yaml = load_yaml!("env.yml");
     let matches = App::from_yaml(yaml).get_matches();
-
-    let strings: Vec<String> = {
-        // Safe to unwrap since we said it is required on clap yaml
-        let values = matches.values_of("STRING").unwrap();
-        let mut v = Vec::new();
-        for value in values {
-            v.push(value.to_owned());
+    let mut kv = HashMap::new();
+    let mut cmd = Vec::new();
+    matches.values_of("OPTIONS").map(|m| {
+        for word in m {
+            let word_str: String = word.to_owned();
+            match word_str.find('=') {
+                Some(index) => {
+                    let (k, v) = word_str.split_at(index);
+                    kv.insert(k.to_owned(), v.get(1..).unwrap_or("").to_owned());
+                }
+                None => {
+                    cmd.push(word_str);
+                }
+            }
         }
-        v
-    };
-
-    match echo(
-        strings,
-        matches.is_present("escape"),
-        matches.is_present("no_newline"),
+    });
+    let mut unset_keys = Vec::new();
+    matches.values_of("REMOVE_VAR").map(|keys| {
+        keys.for_each(|k| unset_keys.push(k.to_owned()));
+    });
+    match env(
+        kv,
+        matches.is_present("IGNORE_ENVIRONMENT"),
+        matches.is_present("NULL_EOL"),
+        cmd,
+        unset_keys,
     ) {
         Ok(_) => (),
         Err(e) => {
@@ -34,102 +43,54 @@ fn main() {
     };
 }
 
-/// Print given `strings` to standard output.
-/// If `scape` true, it also prints the scape codes inside `strings`.
-/// If `no_newline` true, it does not print a newline after.
-fn echo(strings: Vec<String>, escape: bool, no_newline: bool) -> io::Result<()> {
-    let stdout = io::stdout();
-    let mut output = stdout.lock();
-
-    for (i, string) in strings.iter().enumerate() {
-        if i > 0 {
-            write!(output, " ")?;
-        }
-        if escape {
-            let should_stop = print_escape(string, &mut output)?;
-            if should_stop {
-                break;
-            }
-        } else {
-            write!(output, "{}", string)?;
+// run `man env`
+fn env(
+    kv: HashMap<String, String>,
+    ignore_environemnt: bool,
+    null_eol: bool,
+    mut cmd: Vec<String>,
+    unset_keys: Vec<String>,
+) -> io::Result<()> {
+    // Prints each argument on a separate line
+    let mut env_vars = HashMap::new();
+    for (key, value) in env::vars() {
+        if !unset_keys.contains(&key) {
+            env_vars.insert(key, value);
         }
     }
 
-    if !no_newline {
-        writeln!(output)?;
+    if ignore_environemnt {
+        env_vars = HashMap::new();
+    }
+
+    for (key, value) in kv {
+        env_vars.insert(key, value);
+    }
+
+    if cmd.len() > 0 {
+        println!("Spawning program");
+        let cmd_name = cmd.remove(0);
+        println!("{} ", cmd_name);
+        Command::new(cmd_name.clone())
+            .env_clear()
+            .args(cmd)
+            .envs(&env_vars)
+            .status()
+            .expect(&format!("{} failed to start.", cmd_name));
+        println!("done spawining..");
+    } else {
+        for (key, value) in env_vars.iter() {
+            print!("{}={}", key, value);
+            if !null_eol {
+                print!("\n");
+            }
+        }
+
+        if !null_eol {
+            // Let's be polite, and let the shell prompt start on a new line
+            println!("")
+        }
     }
 
     Ok(())
-}
-
-/// Parse a `input` code from `base` code to a UTF-8 char.
-/// The `max_digits` limits how many digits the `input` code can have.
-fn parse_code(
-    input: &mut Peekable<Chars>,
-    base: u32,
-    max_digits: u32,
-    bits_per_digit: u32,
-) -> Option<char> {
-    use std::char::from_u32;
-
-    let mut ret = 0x8000_0000;
-    for _ in 0..max_digits {
-        match input.peek().and_then(|c| c.to_digit(base)) {
-            Some(n) => ret = (ret << bits_per_digit) | n,
-            None => break,
-        }
-        input.next();
-    }
-    from_u32(ret)
-}
-
-/// Print the scape codes from `string`.
-/// `output` is where it is going to be printed.
-fn print_escape(string: &str, mut output: impl Write) -> io::Result<bool> {
-    let mut stop = false;
-    let mut buff = ['\\'; 2];
-    let mut iter = string.chars().peekable();
-    while let Some(mut c) = iter.next() {
-        let mut start_at = 1;
-
-        if c == '\\' {
-            if let Some(n) = iter.next() {
-                c = match n {
-                    '\\' => '\\',
-                    'a' => '\x07',
-                    'b' => '\x08',
-                    'c' => {
-                        stop = true;
-                        break;
-                    }
-                    'e' => '\x1b',
-                    'f' => '\x0c',
-                    'n' => '\n',
-                    'r' => '\r',
-                    't' => '\t',
-                    'v' => '\x0b',
-                    'x' => parse_code(&mut iter, 16, 2, 4).unwrap_or_else(|| {
-                        start_at = 0;
-                        n
-                    }),
-                    '0' => parse_code(&mut iter, 8, 3, 3).unwrap_or_else(|| {
-                        start_at = 0;
-                        n
-                    }),
-                    _ => {
-                        start_at = 0;
-                        n
-                    }
-                }
-            }
-        }
-
-        buff[1] = c;
-
-        for ch in &buff[start_at..] {
-            write!(output, "{}", ch)?;
-        }
-    }
-
-    Ok(stop)
 }
