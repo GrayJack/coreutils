7e4f6c8d514982c963183a26150ea70feb6d8e3e
Jonas Geschke <mail@yonny.de>
2019-10-01 18:16:38 +0200

    Date: added utc and date arguments

--- date/src/main.rs
+++ date/src/main.rs
@@ -5,30 +5,56 @@ use std::{
     process,
 };
 
-use chrono::{DateTime, Local};
+use chrono::{DateTime, Local, NaiveDateTime, Utc};
 
 use clap::{load_yaml, App};
 
 fn main() {
     let yaml = load_yaml!("date.yml");
-    let _matches = App::from_yaml(yaml).get_matches();
+    let matches = App::from_yaml(yaml).get_matches();
 
-    match date() {
+    let is_utc = matches.is_present("utc");
+    let date_value = matches.value_of("date");
+
+    match date(is_utc, date_value) {
         Ok(_) => (),
         Err(e) => {
-            eprintln!("date: Failed to write to stdout.\n{}", e);
+            eprintln!("Date: Failed to write to stdout.\n{}", e);
             process::exit(1);
         }
     };
 }
 
-fn date() -> io::Result<()> {
-    let local: DateTime<Local> = Local::now();
-
+/// prints the local datetime.
+/// If `is_utc` is true, the datetime is printed in universal time.
+/// If `date` is Some it will get parsed and printed out instead of the current datetime.
+fn date(is_utc: bool, date: Option<&str>) -> io::Result<()> {
     let stdout = io::stdout();
     let mut output = stdout.lock();
 
-    write!(output, "{}", local.format("%a %b %e %k:%M:%S %Z %Y"))?;
+    let naive_datetime: NaiveDateTime;
+
+    if let Some(date_str) = date {
+        let parsed_date = NaiveDateTime::parse_from_str(date_str, "%m%d%H%M%Y");
+
+        match parsed_date {
+            Ok(d) => naive_datetime = d,
+            Err(e) => {
+                eprintln!("Date: Could not parse given date.\n{}", e);
+                process::exit(1);
+            }
+        }
+    } else {
+        if is_utc {
+            let utc_datetime: DateTime<Utc> = Utc::now();
+            naive_datetime = utc_datetime.naive_utc();
+        } else {
+            let local_datetime: DateTime<Local> = Local::now();
+            naive_datetime = local_datetime.naive_local();
+        }
+    }
+
+    write!(output, "{}", naive_datetime.format("%a %b %e %k:%M:%S %Y"))?;
 
     Ok(())
 }
