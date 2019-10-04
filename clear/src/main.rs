274dccf6d5a90bc07195285cbefa636b69117a38
Ashwin-A-K <ashwinkumar3098@gmail.com>
2019-10-03 21:52:52 -0700

    clear : added -x option

--- clear/src/main.rs
+++ clear/src/main.rs
@@ -1,6 +1,38 @@
-use std::io::{stdout, Write};
+use std::{
+    io::{stdout, Write},
+    process,
+};
+
+use clap::{load_yaml, App};
 
 fn main() {
-    let stdout = stdout();
-    let _ = stdout.lock().write(b"\x1b[2J\x1b[H");
+    let yaml = load_yaml!("clear.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    // let t = matches.is_present("-T $TERM");
+    let x = matches.is_present("x");
+
+    if let (/*false,*/ false) = (/*t,*/ x) {
+        let _ = stdout().lock().write(b"\x1b[3J\x1b[H\x1b[2J");
+        process::exit(1);
+    }
+
+    // if t {
+    //     let _ = stdout().lock().write(b"\x1b[H\x1b[2J");
+    //     process::exit(1);
+    // } else
+    if x {
+        let _ = stdout().lock().write(b"\x1b[H\x1b[2J");
+        process::exit(1);
+    } else {
+        println!(
+            "Usage: clear [options]
+
+Options:
+-T TERM     use this instead of $TERM
+-V          print curses-version
+-x          do not try to clear scrollback"
+        );
+        process::exit(1);
+    }
 }
