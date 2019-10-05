286cb33f189662e154c2327f1734d45d480222fb
Francis Murillo <francisavmurillo@gmail.com>
2019-10-05 11:08:58 +0800

    seq: Add custom Number type for precision safety

--- seq/src/main.rs
+++ seq/src/main.rs
@@ -1,51 +1,130 @@
-use std::process;
+#![allow(clippy::suspicious_arithmetic_impl)]
+use std::{
+    cmp::Ordering,
+    fmt,
+    ops::{Add, AddAssign},
+    process,
+    str::FromStr
+};
 
 use clap::{load_yaml, App, AppSettings};
+use rust_decimal::Decimal;
 
-#[derive(Copy, Clone, Debug)]
-pub struct Seq {
-    current: f64,
-    stop: f64,
-    step: f64,
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum Number {
+    NegInf,
+    PosInf,
+    NaN,
+    Num(Decimal)
 }
 
-impl Iterator for Seq {
-    type Item = f64;
+use std::cmp::Ordering::*;
+use Number::*;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        if (self.step.is_sign_positive() && self.current <= self.stop)
-            || (self.step.is_sign_negative() && self.current >= self.stop)
-        {
-            let result = self.current;
+impl Add for Number {
+    type Output = Self;
 
-            self.current += self.step;
-
-            Some(result)
-        } else {
-            None
+    fn add(self, other: Self) -> Self {
+        match (self, other) {
+            (NaN, _) | (_, NaN) => NaN,
+            (Num(left), Num(right)) => match left.checked_add(right) {
+                Some(next_dec) => Num(next_dec),
+                None => {
+                    if left.is_sign_positive() && right.is_sign_positive() {
+                        PosInf
+                    } else if left.is_sign_negative() && right.is_sign_negative() {
+                        NegInf
+                    } else {
+                        NaN
+                    }
+                },
+            },
+            (PosInf, PosInf) | (PosInf, Num(_)) | (Num(_), PosInf) => PosInf,
+            (NegInf, NegInf) | (NegInf, Num(_)) | (Num(_), NegInf) => NegInf,
+            (PosInf, NegInf) | (NegInf, PosInf) => NaN
         }
     }
 }
 
-fn parse_number(text: &str) -> Result<f64, ()> {
-    text.parse::<f64>().map_err(|_| ())
+impl AddAssign for Number {
+    fn add_assign(&mut self, other: Self) { *self = *self + other; }
 }
 
-fn check_nan(value: f64) -> Result<f64, ()> {
-    if value.is_nan() {
-        Err(())
-    } else {
-        Ok(value)
+impl PartialOrd for Number {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match (self, other) {
+            (Num(left_dec), Num(right_dec)) => Some(left_dec.cmp(right_dec)),
+            (PosInf, PosInf) | (NegInf, NegInf) | (NaN, NaN) => Some(Equal),
+            (NaN, _) | (_, NaN) => None,
+            (NegInf, PosInf) | (Num(_), PosInf) | (NegInf, Num(_)) => Some(Less),
+            (PosInf, NegInf) | (PosInf, Num(_)) | (Num(_), NegInf) => Some(Greater)
+        }
     }
 }
 
-fn check_zero(value: f64) -> Result<f64, ()> {
-    if value == 0.0 {
-        Err(())
-    } else {
-        Ok(value)
+impl From<f64> for Number {
+    fn from(number: f64) -> Self {
+        match number {
+            value if value.is_infinite() => {
+                if value.is_sign_positive() {
+                    PosInf
+                } else {
+                    NegInf
+                }
+            },
+            value if value.is_finite() => Num(Decimal::from_str(&value.to_string()).unwrap()),
+            _ => NaN
+        }
     }
 }
+
+impl fmt::Display for Number {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self {
+            PosInf => write!(f, "inf"),
+            NegInf => write!(f, "-inf"),
+            NaN => write!(f, "nan"),
+            Num(value) => write!(f, "{}", value)
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Seq {
+    current: Number,
+    step:    Number,
+    stop:    Number
+}
+
+impl Iterator for Seq {
+    type Item = Number;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.step {
+            step @ PosInf | step @ NegInf => Some(step + self.current),
+            Num(step_dec) => {
+                if (step_dec.is_sign_positive() && self.current <= self.stop)
+                    || (step_dec.is_sign_negative() && self.current >= self.stop)
+                {
+                    let result = self.current;
+
+                    self.current += self.step;
+
+                    Some(result)
+                } else {
+                    None
+                }
+            },
+            NaN => Some(NaN)
+        }
+    }
+}
+
+fn parse_number(text: &str) -> Result<f64, ()> { text.parse::<f64>().map_err(|_| ()) }
+
+fn check_nan(value: f64) -> Result<f64, ()> { if value.is_nan() { Err(()) } else { Ok(value) } }
+
+fn check_zero(value: f64) -> Result<f64, ()> { if value == 0.0 { Err(()) } else { Ok(value) } }
 
 fn argument_error(field: &str) -> impl Fn(()) -> f64 + '_ {
     move |_| {
@@ -69,7 +148,7 @@ fn main() {
     let (raw_first, raw_increment, raw_last) = match (
         matches.value_of("FIRST"),
         matches.value_of("SECOND"),
-        matches.value_of("THIRD"),
+        matches.value_of("THIRD")
     ) {
         (Some(last), None, None) => (DEFAULT_FIRST, DEFAULT_INCREMENT, last),
         (Some(first), Some(last), None) => (first, DEFAULT_INCREMENT, last),
@@ -94,9 +173,9 @@ fn main() {
         .unwrap_or_else(argument_error("INCREMENT"));
 
     let iter = Seq {
-        current: first,
-        stop: last,
-        step: increment,
+        current: Number::from(first),
+        stop:    Number::from(last),
+        step:    Number::from(increment)
     };
 
     for index in iter {
