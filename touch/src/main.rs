76e10ee7eb9630e849cc241651a89c1527920203
Michel Kansou <michel.kansou@outlook.fr>
2019-10-09 15:36:17 +0200

    Touch: add date parsing, add unit tests, add chrono crate

--- touch/src/main.rs
+++ touch/src/main.rs
@@ -1,3 +1,4 @@
+use chrono::NaiveDateTime;
 use clap::{load_yaml, App, ArgMatches};
 use filetime::{set_file_atime, set_file_mtime, FileTime};
 use std::fs::{metadata, File};
@@ -9,6 +10,7 @@ fn main() {
     let yaml = load_yaml!("touch.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
+    // get files list in argument
     let files = if matches.is_present("FILE") {
         matches.values_of("FILE").unwrap().collect::<Vec<_>>()
     } else {
@@ -16,13 +18,7 @@ fn main() {
     };
 
     if files.len() > 0 {
-        match touch(&matches, files) {
-            Ok(_) => (),
-            Err(e) => {
-                eprintln!("touch: Failed to write to stdout.\n{}", e);
-                process::exit(1);
-            }
-        };
+        touch(&matches, files).unwrap();
     } else {
         eprintln!("touch: a file name is required.\n");
         process::exit(1);
@@ -31,6 +27,7 @@ fn main() {
 
 fn touch(matches: &ArgMatches, files: Vec<&str>) -> Result<()> {
     for filename in files {
+        // if file already exist in the current directory
         let file_metadata = metadata(&filename);
         let no_create = !matches.is_present("nocreate");
         if file_metadata.is_err() && no_create {
@@ -39,8 +36,20 @@ fn touch(matches: &ArgMatches, files: Vec<&str>) -> Result<()> {
                 Err(e) => eprintln!("touch: Failed to create file {} : {}", filename, e),
             }
         } else {
-            let newfile_time = FileTime::from_system_time(SystemTime::now());
-            update_time(matches, &filename, newfile_time);
+            let date = matches.is_present("date");
+            if date {
+                let date_value = matches.value_of("date").unwrap();
+                let native_date =
+                    NaiveDateTime::parse_from_str(&date_value, "%Y-%m-%d %H:%M:%S").unwrap();
+                let newfile_time = FileTime::from_unix_time(
+                    native_date.timestamp(),
+                    native_date.timestamp_subsec_millis(),
+                );
+                update_time(matches, &filename, newfile_time);
+            } else {
+                let newfile_time = FileTime::from_system_time(SystemTime::now());
+                update_time(matches, &filename, newfile_time);
+            }
         }
     }
     Ok(())
@@ -49,11 +58,12 @@ fn touch(matches: &ArgMatches, files: Vec<&str>) -> Result<()> {
 fn update_time(matches: &ArgMatches, path: &str, filetime: FileTime) {
     let access_time = matches.is_present("accesstime");
     let modification = matches.is_present("modification");
-    let time = matches.value_of("time").unwrap();
+    let time = matches.is_present("time");
+    let time_value = matches.value_of("time");
 
-    if access_time || time == "atime" {
+    if access_time || time && time_value.unwrap() == "atime" {
         update_access_time(&path, filetime);
-    } else if modification || time == "mtime" {
+    } else if modification || time && time_value.unwrap() == "mtime" {
         update_modification_time(&path, filetime);
     } else {
         update_access_time(&path, filetime);
@@ -82,18 +92,132 @@ fn update_modification_time(path: &str, filetime: FileTime) {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs::remove_file;
 
     #[test]
     fn touch_create_empty_files() {
         let matches = ArgMatches::new();
-        let files = vec!["file1.js", "file2.js"];
+        let files = vec!["file1.rs", "file2.rs"];
 
-        match touch(&matches, files) {
-            Ok(_) => (),
-            Err(e) => {
-                eprintln!("touch: Failed to write to stdout.\n{}", e);
-                process::exit(1);
-            }
-        };
+        touch(&matches, files).unwrap();
+
+        assert_eq!(metadata("file1.rs").is_ok(), true);
+        assert_eq!(metadata("file2.rs").is_ok(), true);
+
+        remove_test_files().unwrap();
+    }
+
+    #[test]
+    fn touch_update_existing_files() {
+        let matches = ArgMatches::new();
+        let files = vec!["file1.rs", "file2.rs"];
+
+        File::create("file1.rs").unwrap();
+
+        let mut file1_metadata = metadata("file1.rs").unwrap();
+        let file1_mtime = FileTime::from_last_modification_time(&file1_metadata);
+        let file1_atime = FileTime::from_last_access_time(&file1_metadata);
+
+        //update file1 and create file2
+        touch(&matches, files).unwrap();
+
+        file1_metadata = metadata("file1.rs").unwrap();
+        let new_file1_mtime = FileTime::from_last_modification_time(&file1_metadata);
+        let new_file1_atime = FileTime::from_last_access_time(&file1_metadata);
+        // check that file1 modification time has changed
+        assert_ne!(file1_mtime, new_file1_mtime);
+
+        //check that file1 access time has changed
+        assert_ne!(file1_atime, new_file1_atime);
+        remove_test_files().unwrap();
+    }
+
+    #[test]
+    fn touch_update_only_access_time() {
+        let yaml = load_yaml!("touch.yml");
+        let matches = App::from_yaml(yaml).get_matches_from(vec!["touch", "-a", "file1.rs"]);
+
+        let files = vec!["file1.rs", "file2.rs"];
+
+        File::create("file1.rs").unwrap();
+
+        let mut file1_metadata = metadata("file1.rs").unwrap();
+        let file1_mtime = FileTime::from_last_modification_time(&file1_metadata);
+        let file1_atime = FileTime::from_last_access_time(&file1_metadata);
+
+        //update file1 and create file2
+        touch(&matches, files).unwrap();
+
+        file1_metadata = metadata("file1.rs").unwrap();
+        let new_file1_mtime = FileTime::from_last_modification_time(&file1_metadata);
+        let new_file1_atime = FileTime::from_last_access_time(&file1_metadata);
+        // check that file1 modification time didn't changed
+        assert_eq!(file1_mtime, new_file1_mtime);
+
+        //check that file1 access time has changed
+        assert_ne!(file1_atime, new_file1_atime);
+        remove_test_files().unwrap();
+    }
+
+    #[test]
+    fn touch_update_only_modification_time() {
+        let yaml = load_yaml!("touch.yml");
+        let matches = App::from_yaml(yaml).get_matches_from(vec!["touch", "-m", "file1.rs"]);
+
+        let files = vec!["file1.rs", "file2.rs"];
+
+        File::create("file1.rs").unwrap();
+
+        let mut file1_metadata = metadata("file1.rs").unwrap();
+        let file1_mtime = FileTime::from_last_modification_time(&file1_metadata);
+        let file1_atime = FileTime::from_last_access_time(&file1_metadata);
+
+        //update file1 and create file2
+        touch(&matches, files).unwrap();
+
+        file1_metadata = metadata("file1.rs").unwrap();
+        let new_file1_mtime = FileTime::from_last_modification_time(&file1_metadata);
+        let new_file1_atime = FileTime::from_last_access_time(&file1_metadata);
+        // check that file1 modification time has chang
+        assert_ne!(file1_mtime, new_file1_mtime);
+
+        //check that file1 access time didn't changed
+        assert_eq!(file1_atime, new_file1_atime);
+        remove_test_files().unwrap();
+    }
+
+    #[test]
+    fn touch_update_time_with_date() {
+        let yaml = load_yaml!("touch.yml");
+        let matches = App::from_yaml(yaml).get_matches_from(vec![
+            "touch",
+            "-d=2009-01-03 03:13:00",
+            "file1.rs",
+        ]);
+
+        let files = vec!["file1.rs", "file2.rs"];
+
+        File::create("file1.rs").unwrap();
+
+        //update file1 and create file2
+        touch(&matches, files).unwrap();
+
+        let file1_metadata = metadata("file1.rs").unwrap();
+        let file1_mtime = FileTime::from_last_modification_time(&file1_metadata);
+
+        let time =
+            NaiveDateTime::from_timestamp(file1_mtime.unix_seconds(), file1_mtime.nanoseconds());
+        // check modification and access time is equal 2009-01-03 03:13:00
+        assert_eq!(
+            time,
+            NaiveDateTime::parse_from_str("2009-01-03 03:13:00", "%Y-%m-%d %H:%M:%S").unwrap()
+        );
+        remove_test_files().unwrap();
+    }
+
+    fn remove_test_files() -> Result<()> {
+        remove_file("file1.rs")?;
+        remove_file("file2.rs")?;
+        Ok(())
     }
 }
