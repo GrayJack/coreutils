75af778a092d28db1889467cbd972dcbb5f5d2b0
GrayJack <gr41.j4ck@gmail.com>
2019-10-09 12:09:12 -0300

    Clear: Clippy suggestion

--- clear/src/main.rs
+++ clear/src/main.rs
@@ -9,30 +9,24 @@ fn main() {
     let yaml = load_yaml!("clear.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    // let t = matches.is_present("-T $TERM");
-    let x = matches.is_present("x");
+    let x_flag = matches.is_present("x");
 
-    if let (/*false,*/ false) = (/*t,*/ x) {
-        let _ = stdout().lock().write(b"\x1b[3J\x1b[H\x1b[2J");
-        process::exit(1);
+    if x_flag {
+        match stdout().lock().write(b"\x1b[H\x1b[3J") {
+            Ok(_) => (),
+            Err(err) => {
+                eprintln!("clear: failed to execute: {}", err);
+                process::exit(1);
+            },
+        };
+        return;
     }
 
-    // if t {
-    //     let _ = stdout().lock().write(b"\x1b[H\x1b[2J");
-    //     process::exit(1);
-    // } else
-    if x {
-        let _ = stdout().lock().write(b"\x1b[H\x1b[2J");
-        process::exit(1);
-    } else {
-        println!(
-            "Usage: clear [options]
-
-Options:
--T TERM     use this instead of $TERM
--V          print curses-version
--x          do not try to clear scrollback"
-        );
-        process::exit(1);
-    }
+    match stdout().lock().write(b"\x1b[3J\x1b[H\x1b[2J") {
+        Ok(_) => (),
+        Err(err) => {
+            eprintln!("clear: failed to execute: {}", err);
+            process::exit(1);
+        },
+    };
 }
