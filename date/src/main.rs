ed36577cfbba4ec74e61a2cdb9df2666e9783c31
Jonas Geschke <mail@yonny.de>
2019-10-12 22:03:53 +0200

    Date: read date and read from seconds and read modified file works now

--- date/src/main.rs
+++ date/src/main.rs
@@ -6,14 +6,12 @@ use std::{
     process,
 };
 
-use chrono::{Local, DateTime, TimeZone, Utc, NaiveDateTime, FixedOffset, Date, ParseResult};
+use chrono::{Local, DateTime, TimeZone, Utc, FixedOffset, NaiveDateTime, NaiveDate};
 
 use clap::{load_yaml, App, ArgMatches};
-use std::string::ParseError;
 use std::path::Path;
-use std::error::Error;
 use std::io::ErrorKind;
-use std::str::FromStr;
+use chrono::format::Fixed;
 
 fn main() {
     let yaml = load_yaml!("date.yml");
@@ -22,7 +20,7 @@ fn main() {
     match date(&matches) {
         Ok(_) => (),
         Err(e) => {
-            eprintln!("Date: Failed to write to stdout.\n{}", e);
+            eprintln!("date: {}", e);
             process::exit(1);
         }
     };
@@ -31,89 +29,103 @@ fn main() {
 /// prints the local datetime.
 /// If `is_utc` is true, the datetime is printed in universal time.
 /// If `date` is Some it will get parsed and printed out instead of the current datetime.
-fn date(args: &ArgMatches) -> io::Result<()> {
+fn date<'a>(args: &ArgMatches) -> Result<(), &'a str> {
     let is_rfc2822 = args.is_present("RFC2822");
     let is_utc = args.is_present("utc");
     let is_date = args.is_present("date");
     let is_read = args.is_present("read");
 
+    let mut datetime: DateTime<Local> = Local::now();
+
     if is_read || is_date {
-        let mut datetime: Option<DateTime<FixedOffset>> = Option::None;
         if is_date {
             let date_str = args.value_of("date").unwrap();
-            if let Ok(format) = build_parse_format(date_str, date_str.chars().count()) {
-                match DateTime::parse_from_str(date_str, &format) {
-                    Ok(d) => datetime = Some(d),
-                    Err(e) => return Err(io::Error::from(ErrorKind::InvalidInput))
-                }
+            match read_date(date_str) {
+                Ok(date) => datetime = date,
+                Err(e) => return Err(e)
             }
         } else {
             let date_str = args.value_of("read").unwrap();
-            let parsed: Result<i32, _> = date_str.trim().parse();
-            let result = match parsed {
-                Ok(_) => parse_seconds(date_str.trim()),
-                Err(_) => parse_file(date_str)
-            };
-
-            if let Ok(date) = result {
-                datetime = Some(date);
+            match read(date_str) {
+                Ok(date) => datetime = date,
+                Err(e) => return Err(e)
             }
         }
+    }
 
-        if is_rfc2822 {
-            format_rfc2822(datetime.unwrap());
-        } else {
-            format_standard(datetime.unwrap());
-        }
-    } else if is_utc {
-        let datetime = Utc::now();
-
-        if is_rfc2822 {
-            format_rfc2822(datetime);
-        } else {
-            format_standard(datetime);
-        }
+    if is_rfc2822 {
+        format_rfc2822(datetime, is_utc);
     } else {
-        let datetime = Local::now();
-
-        if is_rfc2822 {
-            format_rfc2822(datetime);
-        } else {
-            format_standard(datetime);
-        }
+        format_standard(datetime, is_utc);
     }
 
     Ok(())
 }
 
-fn parse_seconds(seconds: &str) -> Result<DateTime<FixedOffset>, io::Error> {
-    match DateTime::parse_from_str(seconds, "%S") {
-        Ok(date) => Ok(date),
+/// Reads datetime from `input`. Could be seconds or a filepath.
+fn read<'a>(input: &str) -> Result<DateTime<Local>, &'a str> {
+    let parsed: Result<i32, _> = input.trim().parse();
+    let result = match parsed {
+        Ok(_) => parse_seconds(input.trim()),
+        Err(_) => parse_file(input)
+    };
+
+    if let Ok(date) = result {
+        return Ok(date);
+    } else {
+        return Err("illegal date time format");
+    }
+}
+
+/// Parses date from `date_str`.
+fn read_date<'a>(date_str: &str) -> Result<DateTime<Local>, &'a str> {
+    if let Ok(format) = build_parse_format(date_str) {
+        match NaiveDateTime::parse_from_str(date_str, &format) {
+            Ok(d) => Ok(TimeZone::from_utc_datetime(&Local, &d)),
+            Err(e) => Err("illegal date time format")
+        }
+    } else {
+        Err("could not parse date")
+    }
+}
+
+/// Return the local DateTime
+fn parse_seconds(seconds: &str) -> Result<DateTime<Local>, io::Error> {
+    //let other: String = Local::now().format("%s").to_string();
+    match NaiveDateTime::parse_from_str(seconds, "%s") {
+        Ok(date) => {
+            //let local = TimeZone::from_local_datetime(&Local, &date).unwrap();
+            let local = TimeZone::from_utc_datetime(&Local, &date);
+            Ok(local)
+        },
         Err(e) => Err(io::Error::new(ErrorKind::InvalidInput, e))
     }
 }
 
-fn parse_file(filename: &str) -> Result<DateTime<FixedOffset>, io::Error> {
-    let path = Path::new(filename) ;
+/// Returns the modified date of `filename`.
+/// Returns `NotFound` if `filename` could not be found.
+fn parse_file(filename: &str) -> Result<DateTime<Local>, io::Error> {
+    let path = Path::new(filename);
+
     if path.exists() {
         let metadata = path.metadata().unwrap();
         let modified = metadata.modified().unwrap();
-        println!("System time is {:?} ", modified);
-        let datetime: DateTime<Utc> = DateTime::from(modified); // TODO this should be local time
-        let datetime_fixed: DateTime<FixedOffset> = DateTime::from(datetime);
-        return Ok(datetime_fixed);
+        let datetime: DateTime<Local> = DateTime::from(modified);
+
+        return Ok(datetime);
     } else {
         return Err(io::Error::from(ErrorKind::NotFound));
     }
 }
 
-fn build_parse_format(date: &str, date_str_len: usize) -> Result<String, ()> {
+/// Builds the correct datetime format string to parse `date`.
+fn build_parse_format(date: &str) -> Result<String, ()> {
     // format is [[[[[cc]yy]mm]dd]HH]MM[.ss]
     let mut result: Result<String, ()> = Err(());
     let mut format = vec![' ', ' ', ' ', ' ', ' ', ' ', ' '];
-    let mut len = date_str_len;
+    let mut len = date.chars().count();
 
-    if date.contains("\\.") {
+    if date.contains(".") {
         format[6] = 'S';
         len -= 3;
     }
@@ -141,8 +153,10 @@ fn build_parse_format(date: &str, date_str_len: usize) -> Result<String, ()> {
     let spliced_format = format[..6].iter();
 
     for chr in spliced_format {
-        format_str.push_str("%");
-        format_str.push_str(&chr.to_string());
+        if !chr.eq(&' '){
+            format_str.push_str("%");
+            format_str.push_str(&chr.to_string());
+        }
     }
 
     if format[6] != ' ' {
@@ -153,19 +167,23 @@ fn build_parse_format(date: &str, date_str_len: usize) -> Result<String, ()> {
 }
 
 /// displays `datetime` in rfc2822 format
-fn format_rfc2822<Tz: TimeZone>(datetime: DateTime<Tz>) where Tz::Offset : fmt::Display {
+fn format_rfc2822<Tz: TimeZone>(datetime: DateTime<Tz>, is_utc: bool) where Tz::Offset : fmt::Display {
     let format_str = "%a, %d %b %Y %T %z";
-    format(datetime, format_str);
+    format(datetime, format_str, is_utc);
 }
 
 /// displays `datetime` standard format `"%a %b %e %k:%M:%S %Z %Y"`
-fn format_standard<Tz: TimeZone>(datetime: DateTime<Tz>) where Tz::Offset : fmt::Display {
+fn format_standard<Tz: TimeZone>(datetime: DateTime<Tz>, is_utc: bool) where Tz::Offset : fmt::Display {
     let format_str = "%a %b %e %k:%M:%S %Z %Y"; // <- %Z should print the name of the timezone (only works for UTC)
     // problem is in chrono lib: https://github.com/chronotope/chrono/issues/288
-    format(datetime, format_str);
+    format(datetime, format_str, is_utc);
 }
 
 /// displays `datetime` with given `output_format`
-fn format<Tz: TimeZone >(datetime: DateTime<Tz>, output_format: &str) where Tz::Offset : fmt::Display {
-    println!("{}", datetime.format(output_format));
+fn format<Tz: TimeZone >(datetime: DateTime<Tz>, output_format: &str, is_utc: bool) where Tz::Offset : fmt::Display {
+    if is_utc {
+        println!("{}", datetime.with_timezone(&Utc).format(output_format));
+    } else {
+        println!("{}", datetime.with_timezone(&Local).format(output_format));
+    }
 }
