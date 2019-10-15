0ffa563fb9c12723715ab7caa1fbe6dbd0df58fa
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2019-10-14 23:56:37 -0300

    mkdir: add --mode option

--- mkdir/src/main.rs
+++ mkdir/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::os::unix::fs::PermissionsExt;
 
 use clap::{load_yaml, App};
 
@@ -14,6 +15,8 @@ fn main() {
     let directories = matches.values_of("DIRECTORY").unwrap();
     let verbose = matches.is_present("verbose");
     let parents = matches.is_present("parents");
+    let has_mode = matches.is_present("mode");
+    let mode = matches.value_of("mode").unwrap();
 
     let mkdir = { 
         if parents { 
@@ -25,7 +28,21 @@ fn main() {
     
     for d in directories {
         match mkdir(d) {
-            Ok(_) => if verbose { log(format!("created directory '{}'", d)) },
+            Ok(_) => {
+                if verbose { log(format!("created directory '{}'", d)) };
+                if has_mode {
+                    match fs::metadata(d) {
+                        Ok(v) => {
+                            let mut perms = v.permissions();
+                            let umode: u32 = mode.parse().unwrap();
+                            perms.set_mode(umode);
+                        }
+                        Err(e) => {
+                            log(format!("{}", e));
+                        }
+                    }
+                }
+            }
             Err(e) => log(format!("cannot create directory '{}': {}", d, e))
         }
     }
