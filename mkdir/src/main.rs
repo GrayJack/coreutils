fc2f1fd0f67f793ec994d5fc56a17799c7afd904
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2019-10-14 22:58:22 -0300

    mkdir: change to clap crate

--- mkdir/src/main.rs
+++ mkdir/src/main.rs
@@ -1,25 +1,32 @@
-use std::env;
 use std::fs;
+
+use clap::{load_yaml, App};
+
 
 fn log<S: Into<String>>(msg: S) {
     println!("mkdir: {}", msg.into());
 }
 
-
 fn main() {
+    let yaml = load_yaml!("mkdir.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+    
+    let directories = matches.values_of("DIRECTORY").unwrap();
+    let verbose = matches.is_present("verbose");
+    let parents = matches.is_present("parents");
 
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        log("missing operand\nTry 'mkdir --help' for more information.");
-    }
-
-    if args.len() == 2 {
-        let dir_path = &args[1];
-        match fs::create_dir(dir_path) {
-            Ok(_) => (),
-            Err(e) => {
-                log(format!("cannot create directory '{}': {}", dir_path, e))
-            }
+    let mkdir = { 
+        if parents { 
+            fs::create_dir_all
+        } else {
+            fs::create_dir
+        }
+    };
+    
+    for d in directories {
+        match mkdir(d) {
+            Ok(_) => if verbose { log(format!("created directory '{}'", d)) },
+            Err(e) => log(format!("cannot create directory '{}': {}", d, e))
         }
     }
 }
