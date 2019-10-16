0e1debc47119e52041569dcd02eecda41403a419
Gabriele Bianchet-David <gbianchetdavid@ebsco.com>
2019-10-16 12:39:54 -0400

    refactor: fixed formatting

--- mv/src/main.rs
+++ mv/src/main.rs
@@ -1,4 +1,4 @@
-use clap::{load_yaml, App, ArgMatches, AppSettings::ColoredHelp};
+use clap::{load_yaml, App, AppSettings::ColoredHelp, ArgMatches};
 use coreutils_core::{backup::*, input::*};
 use std::{
     fs,
