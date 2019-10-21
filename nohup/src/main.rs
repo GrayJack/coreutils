34b4ed0866b05364679d255f6f5bbc55f06f3687
Federico Ponzi <isaacisback92@gmail.com>
2019-10-22 00:23:38 +0200

    nohup: Adds initial implementation

--- nohup/src/main.rs
+++ nohup/src/main.rs
@@ -1,17 +1,52 @@
-use clap::{load_yaml, App};
+extern crate signal_hook;
 
-fn main() {
+use clap::{load_yaml, App};
+use coreutils_core::{file_descriptor::FileDescriptor, tty::isatty};
+use std::{
+    env,
+    fs::{File, OpenOptions},
+    io::Error,
+    process::{Command, Stdio},
+};
+
+fn main() -> Result<(), Error> {
     let yaml = load_yaml!("nohup.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    let string = if matches.is_present("STRING") {
-        let inputs = matches.values_of("STRING").unwrap();
-        inputs.fold(String::new(), |res, s| res + s + " ")
-    } else {
-        "y".to_string()
-    };
+    let args_val = matches.values_of("COMMAND").unwrap();
+    let mut args = args_val.into_iter().map(|x| x.to_owned()).collect::<Vec<String>>();
+    let command_name = args.remove(0);
 
-    loop {
-        println!("{}", string);
+    let mut command = &mut Command::new(command_name);
+    // If standard input is a terminal, redirect it from an unreadable file.
+    if isatty(FileDescriptor::StdIn) {
+        command = command.stdin(Stdio::null());
     }
+    let mut open_opts = OpenOptions::new();
+    open_opts.write(true).create(true).append(true);
+    let get_stdout = || -> File {
+        open_opts.open("nohup.out").unwrap_or_else(|_| {
+            for (key, value) in env::vars() {
+                if key == "HOME" {
+                    return open_opts.open(value + "nohup.out").unwrap();
+                }
+            }
+            panic!("There is no $HOME variable in the environment.");
+        })
+    };
+    let _signal_handler = unsafe {
+        signal_hook::register(signal_hook::SIGHUP, || {
+            //ignore
+        })
+    }?;
+    if isatty(FileDescriptor::StdOut) {
+        //Try to open in write append nohup.out else open $HOME/nohup.out
+        command = command.stdout(get_stdout());
+    }
+    // If standard error is a terminal, redirect it to standard output.
+    if isatty(FileDescriptor::StdErr) {
+        command = command.stderr(get_stdout());
+    }
+    command.status().expect("Error while invoking the command.");
+    Ok(())
 }
