99dbdd5f31e572d338d147b5b59768fc40341a95
Jonas Geschke <mail@yonny.de>
2019-10-22 09:23:58 +0200

    Date: formatted settime

--- coreutils_core/src/settime.rs
+++ coreutils_core/src/settime.rs
@@ -1,13 +1,11 @@
-use std::{ptr, io};
 use super::types::TimeVal;
+use std::{io, ptr};
 
 /// Wrapper function for `libc::settimeofday`
 pub fn settimeofday(timeval: TimeVal) -> io::Result<()> {
     let result = unsafe { libc::settimeofday(&timeval as *const TimeVal, ptr::null()) };
     match result {
         0 => Ok(()),
-        _ => {
-            Err(io::Error::last_os_error())
-        }
+        _ => Err(io::Error::last_os_error()),
     }
 }
