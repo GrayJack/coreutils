4aa648089fb85c3de722a1e6e3407bb8bf6fba9d
Federico Ponzi <isaacisback92@gmail.com>
2019-10-26 23:21:52 +0200

    fmt

--- mkfifo/src/main.rs
+++ mkfifo/src/main.rs
@@ -1,13 +1,14 @@
 use clap::{load_yaml, App, AppSettings::ColoredHelp};
-use std::{process};
 use coreutils_core::mkfifo::mkfifo;
+use std::process;
 
 fn main() {
     let yaml = load_yaml!("mkfifo.yml");
     let matches = App::from_yaml(yaml).settings(&[ColoredHelp]).get_matches();
     let filepath = matches.value_of("NAME").expect("No filename provided.");
     let mode = matches.value_of("mode").unwrap();
-    let mode: u32 = u32::from_str_radix(mode, 8).unwrap_or_else(|_| panic!("Mode '{}' is not an octal number.", mode));
+    let mode: u32 = u32::from_str_radix(mode, 8)
+        .unwrap_or_else(|_| panic!("Mode '{}' is not an octal number.", mode));
     match mkfifo(filepath, mode.into()) {
         Ok(_) => (),
         Err(e) => {
@@ -16,4 +17,3 @@ fn main() {
         },
     }
 }
-
