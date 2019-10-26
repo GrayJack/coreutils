51c9dee01bcc32bfc736097a108a2801ce15eac4
Federico Ponzi <isaacisback92@gmail.com>
2019-10-26 23:21:21 +0200

    mkfifo: working implementation.

---
+++ coreutils_core/src/mkfifo.rs
@@ -0,0 +1,45 @@
+use libc::{self, mode_t, EACCES, EEXIST, ENOENT};
+use std::io::Error;
+use std::{ffi::CString, io, path::Path};
+
+///
+/// Mkfifo wrapper around libc's
+/// Inspired by crate `unix_named_pipe`
+pub fn mkfifo(filepath: &str, mode: u32) -> io::Result<()> {
+    let path: &Path = Path::new(filepath).as_ref();
+    let path = CString::new(path.to_str().unwrap())?;
+    let result = unsafe { libc::mkfifo(path.as_ptr(), mode as mode_t) };
+
+    let result: i32 = result;
+    if result == 0 {
+        return Ok(());
+    }
+
+    let error = errno::errno();
+    match Error::last_os_error().raw_os_error().unwrap() {
+        EACCES => {
+            return Err(io::Error::new(
+                io::ErrorKind::PermissionDenied,
+                format!("could not open {:?}: {}", path, error),
+            ));
+        },
+        EEXIST => {
+            return Err(io::Error::new(
+                io::ErrorKind::AlreadyExists,
+                format!("could not open {:?}: {}", path, error),
+            ));
+        },
+        ENOENT => {
+            return Err(io::Error::new(
+                io::ErrorKind::NotFound,
+                format!("could not open {:?}: {}", path, error),
+            ));
+        },
+        _ => {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                format!("could not open {:?}: {}", path, error),
+            ));
+        },
+    }
+}
