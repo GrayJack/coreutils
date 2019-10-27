eb4ab71710ba56c2febb9b084ab1d9df01cb9dfe
Federico Ponzi <isaacisback92@gmail.com>
2019-10-27 19:57:24 +0100

    Fix wrong exit status

--- mkfifo/src/main.rs
+++ mkfifo/src/main.rs
@@ -8,7 +8,10 @@ fn main() {
     let filepath = matches.value_of("NAME").expect("No filename provided.");
     let mode = matches.value_of("mode").unwrap();
     let mode: u32 = u32::from_str_radix(mode, 8)
-        .unwrap_or_else(|_| panic!("Mode '{}' is not an octal number.", mode));
+        .unwrap_or_else(|_| {
+            eprintln!("Mode '{}' is not an octal number.", mode);
+            process::exit(1);
+        });
     match mkfifo(filepath, mode.into()) {
         Ok(_) => (),
         Err(e) => {
