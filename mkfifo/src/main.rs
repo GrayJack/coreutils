fc49852b49bfeb2618e48be9f6e64b693fa32b0f
Federico Ponzi <isaacisback92@gmail.com>
2019-10-27 19:58:27 +0100

    fmt

--- mkfifo/src/main.rs
+++ mkfifo/src/main.rs
@@ -7,11 +7,10 @@ fn main() {
     let matches = App::from_yaml(yaml).settings(&[ColoredHelp]).get_matches();
     let filepath = matches.value_of("NAME").expect("No filename provided.");
     let mode = matches.value_of("mode").unwrap();
-    let mode: u32 = u32::from_str_radix(mode, 8)
-        .unwrap_or_else(|_| {
-            eprintln!("Mode '{}' is not an octal number.", mode);
-            process::exit(1);
-        });
+    let mode: u32 = u32::from_str_radix(mode, 8).unwrap_or_else(|_| {
+        eprintln!("Mode '{}' is not an octal number.", mode);
+        process::exit(1);
+    });
     match mkfifo(filepath, mode.into()) {
         Ok(_) => (),
         Err(e) => {
