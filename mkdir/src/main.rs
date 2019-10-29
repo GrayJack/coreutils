91f612686773015bd4033d89772b4b4639a4eba2
GrayJack <gr41.j4ck@gmail.com>
2019-10-29 20:41:07 -0300

    Mkdir: Colorize help

--- mkdir/src/main.rs
+++ mkdir/src/main.rs
@@ -1,10 +1,10 @@
 use std::{fs, os::unix::fs::PermissionsExt, process};
 
-use clap::{load_yaml, App};
+use clap::{load_yaml, App, AppSettings::ColoredHelp};
 
 fn main() {
     let yaml = load_yaml!("mkdir.yml");
-    let matches = App::from_yaml(yaml).get_matches();
+    let matches = App::from_yaml(yaml).settings(&[ColoredHelp]).get_matches();
 
     let directories = matches.values_of("DIRECTORY").unwrap();
     let verbose = matches.is_present("verbose");
