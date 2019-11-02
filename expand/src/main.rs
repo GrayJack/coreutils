e00a80f6629a8c1ccdca89f58e7449a3d53f1623
muskuloes <19honor@gmail.com>
2019-11-02 14:03:51 +0100

    implement expand command

---
+++ expand/src/main.rs
@@ -0,0 +1,140 @@
+use std::{
+    env::current_dir,
+    fs::File,
+    io::{prelude::BufRead, stdin, stdout, BufReader, Write},
+    process,
+};
+
+use clap::{load_yaml, App, AppSettings::ColoredHelp, ArgMatches};
+
+mod tab_stops;
+use tab_stops::*;
+
+fn main() {
+    let yaml = load_yaml!("expand.yml");
+    let matches = App::from_yaml(yaml).settings(&[ColoredHelp]).get_matches();
+    let mut expand = Expand::from_matches(&matches);
+    let cwd = match current_dir() {
+        Ok(path) => path,
+        Err(err) => {
+            eprintln!("expand: error reading current working directory: {}", err);
+            process::exit(1);
+        },
+    };
+
+    let files = match matches.values_of("FILE") {
+        Some(files) => files
+            .map(|file| {
+                if file == "-" {
+                    return String::from("-");
+                }
+
+                file.split_whitespace()
+                    .map(|s| cwd.join(s.to_string()).to_str().unwrap().to_string())
+                    .collect()
+            })
+            .collect(),
+        None => vec!["-".to_string()],
+    };
+
+    let mut stdout = stdout();
+
+    let write_error = |err| {
+        eprintln!("expand: read error: {}", err);
+        process::exit(1);
+    };
+
+    let read_error = |err| {
+        eprintln!("unexpand: read error: {}", err);
+        String::new()
+    };
+
+    for file_path in files {
+        if file_path == "-" {
+            let stdin = stdin();
+            for line in stdin.lock().lines() {
+                stdout
+                    .write_all(expand.expand_line(&line.unwrap_or_else(read_error)).as_bytes())
+                    .unwrap_or_else(write_error);
+                stdout.flush().unwrap_or_else(write_error);
+            }
+        } else {
+            let fd = File::open(file_path).unwrap();
+            let reader = BufReader::new(fd);
+            for line in reader.lines() {
+                stdout
+                    .write_all(expand.expand_line(&line.unwrap_or_else(read_error)).as_bytes())
+                    .unwrap_or_else(write_error);
+                stdout.flush().unwrap_or_else(write_error);
+            }
+        }
+    }
+}
+
+struct Expand {
+    initial:  bool,
+    tabstops: TabStops,
+}
+
+impl Expand {
+    fn from_matches(matches: &ArgMatches) -> Self {
+        let initial = matches.is_present("initial");
+
+        let tabs_str = matches.value_of("tabs");
+        let tabstops = match TabStops::new(tabs_str) {
+            Ok(tab_stops) => {
+                tab_stops
+            },
+            Err(err_message) => {
+                eprintln!("{}", err_message);
+                std::process::exit(1);
+            },
+        };
+
+        Expand { initial, tabstops }
+    }
+
+    fn expand_line(self: &mut Self, line: &str) -> String {
+        let mut convert = true;
+        let mut column = 0;
+        let mut new_line: String = String::new();
+
+
+        for c in line.bytes() {
+            match c {
+                b'\t' => {
+                    let spaces = match self.tabstops.repetable {
+                        Some(t) => t-column % t,
+                        None => {
+                            match self.tabstops.positions.iter().skip_while(|&&t| t <= column).next() {
+                                Some(t) => t-column,
+                                None => 1
+                            }
+                        }
+                    };
+                    column += spaces;
+                    if convert {
+                        new_line.push_str(String::from(" ").repeat(spaces as usize).as_str());
+                    } else {
+                        new_line.push('\t');
+                    }
+                },
+                b'\x08' => {
+                    column = if column > 0 {
+                        column -1
+                    } else {
+                        0
+                    };
+                    new_line.pop();
+                },
+                _ => {
+                    column += 1;
+                    convert &= !self.initial;
+                    new_line.push(c as char);
+                },
+            };
+        }
+        new_line.push_str("\n");
+        new_line
+    }
+}
---
+++ expand/src/tab_stops.rs
@@ -0,0 +1,107 @@
+#[derive(Debug)]
+pub struct TabStops {
+    pub(crate) offset:    Option<usize>,
+    pub(crate) repetable: Option<usize>,
+    pub(crate) positions: Vec<usize>,
+}
+
+const ARG_PARSE_MSG: &str = "unexpand: error parsing arguments";
+
+impl TabStops {
+    pub fn new(tabs_str: Option<&str>) -> Result<Self, String> {
+        match tabs_str {
+            Some(tabs_str) => {
+                if tabs_str == "" {
+                    return Ok(TabStops { offset: None, repetable: Some(8), positions: vec![] });
+                }
+
+                let tabs_str = tabs_str.replace(", ", ",");
+                let mut tabs_vec: Vec<&str> =
+                    tabs_str.split(|c| c == ',' || c == ' ').map(str::trim).collect();
+
+                if tabs_vec.len() == 1 {
+                    let value = tabs_vec[0]
+                        .parse::<usize>()
+                        .map_err(|err| format!("{}: {}", ARG_PARSE_MSG, err))?;
+
+                    if value == 0 {
+                        return Err("unexpand: tab size cannot be 0".to_string());
+                    }
+
+                    return Ok(TabStops {
+                        offset:    None,
+                        repetable: Some(value),
+                        positions: vec![],
+                    });
+                }
+
+                let mut offset: Option<usize> = None;
+                let mut repetable: Option<usize> = None;
+                let last_item = String::from(*tabs_vec.last().unwrap());
+
+                if last_item.contains('+') {
+                    repetable = Some(
+                        tabs_vec.pop().unwrap()[1..]
+                            .parse::<usize>()
+                            .map_err(|err| format!("{}: {}", ARG_PARSE_MSG, err))?,
+                    );
+                    offset = Some(
+                        tabs_vec
+                            .pop()
+                            .unwrap()
+                            .parse::<usize>()
+                            .map_err(|err| format!("{}: {}", ARG_PARSE_MSG, err))?,
+                    );
+                }
+
+                if last_item.contains('/') {
+                    repetable = Some(
+                        last_item[1..]
+                            .parse::<usize>()
+                            .map_err(|err| format!("{}: {}", ARG_PARSE_MSG, err))?,
+                    );
+                    tabs_vec.pop();
+                }
+
+                let mut positions: Vec<usize> = vec![];
+                for tab_val in &tabs_vec {
+                    positions.push(
+                        tab_val
+                            .parse::<usize>()
+                            .map_err(|err| format!("{}: {}", ARG_PARSE_MSG, err))?,
+                    );
+                }
+
+                if !positions.is_empty() {
+                    if positions.contains(&0) {
+                        return Err("unexpand: tab size cannot be 0".to_string());
+                    }
+
+                    for i in 0..(positions.len() - 1) {
+                        if positions[i + 1] <= positions[i] {
+                            return Err("unexpand: tab sizes must be ascending".to_string());
+                        }
+                    }
+
+                    return Ok(TabStops { offset, repetable, positions });
+                }
+
+                Ok(TabStops { offset, repetable, positions: vec![] })
+            },
+            None => Ok(TabStops { offset: None, repetable: Some(8), positions: vec![] }),
+        }
+    }
+
+    pub fn is_tab_stop(self: &Self, column: usize) -> bool {
+        if self.positions.contains(&column) {
+            return true;
+        }
+
+        if self.repetable.is_some() && column % self.repetable.unwrap() == self.offset.unwrap_or(0)
+        {
+            return true;
+        }
+
+        false
+    }
+}
