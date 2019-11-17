64f31326986b96ca1c953443ee176e4d740c6685
GrayJack <gr41.j4ck@gmail.com>
2019-11-17 01:59:58 -0300

    Core: utsname: Impl TryFrom for UtsName and From for utsname

--- coreutils_core/src/utsname.rs
+++ coreutils_core/src/utsname.rs
@@ -1,5 +1,6 @@
 //! Module for system information
 use std::{
+    convert::TryFrom,
     ffi::CStr,
     fmt::{self, Display},
     io,
@@ -7,7 +8,7 @@ use std::{
 };
 
 use bstr::{BStr, BString, ByteSlice};
-use libc::{uname, utsname};
+use libc::{c_char, uname, utsname};
 
 /// A struct that holds several system informations, like the system name, host name, etc.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -40,6 +41,39 @@ impl UtsName {
 
         let uts_name = unsafe { uts_name.assume_init() };
 
+        Ok(UtsName::try_from(uts_name)?)
+    }
+
+    /// Get system name.
+    #[inline]
+    pub fn system_name(&self) -> &BStr { self.sysname.as_bstr() }
+
+    /// Get host name of the machine
+    #[inline]
+    pub fn node_name(&self) -> &BStr { self.nodename.as_bstr() }
+
+    /// Get the release level of the operating system.
+    #[inline]
+    pub fn release(&self) -> &BStr { self.release.as_bstr() }
+
+    /// Get the version level of this release of the operating system.
+    #[inline]
+    pub fn version(&self) -> &BStr { self.version.as_bstr() }
+
+    /// Get the type of the current hardware platform.
+    #[inline]
+    pub fn machine(&self) -> &BStr { self.machine.as_bstr() }
+
+    /// NIS or YP domain name
+    #[inline]
+    #[cfg(any(target_os = "linux", target_os = "fuchsia"))]
+    pub fn domain_name(&self) -> &BStr { self.domainname.as_bstr() }
+}
+
+impl TryFrom<utsname> for UtsName {
+    type Error = io::Error;
+
+    fn try_from(uts_name: utsname) -> Result<Self, Self::Error> {
         let sysname = {
             let name = unsafe { CStr::from_ptr(&uts_name.sysname[0]) };
             BString::from(name.to_bytes())
@@ -81,31 +115,6 @@ impl UtsName {
             domainname,
         })
     }
-
-    /// Get system name.
-    #[inline]
-    pub fn system_name(&self) -> &BStr { self.sysname.as_bstr() }
-
-    /// Get host name of the machine
-    #[inline]
-    pub fn node_name(&self) -> &BStr { self.nodename.as_bstr() }
-
-    /// Get the release level of the operating system.
-    #[inline]
-    pub fn release(&self) -> &BStr { self.release.as_bstr() }
-
-    /// Get the version level of this release of the operating system.
-    #[inline]
-    pub fn version(&self) -> &BStr { self.version.as_bstr() }
-
-    /// Get the type of the current hardware platform.
-    #[inline]
-    pub fn machine(&self) -> &BStr { self.machine.as_bstr() }
-
-    /// NIS or YP domain name
-    #[inline]
-    #[cfg(any(target_os = "linux", target_os = "fuchsia"))]
-    pub fn domain_name(&self) -> &BStr { self.domainname.as_bstr() }
 }
 
 impl Display for UtsName {
@@ -116,5 +125,51 @@ impl Display for UtsName {
             "{} {} {} {} {}",
             self.sysname, self.nodename, self.release, self.version, self.machine
         )
+    }
+}
+
+// Extra traits
+#[cfg(any(
+    target_os = "freebsd",
+    target_os = "netbsd",
+    target_os = "openbsd",
+    target_os = "macos"
+))]
+const UTSNAME_SIZE: usize = 256;
+#[cfg(any(target_os = "solaris"))]
+const UTSNAME_SIZE: usize = 257;
+#[cfg(any(target_os = "linux", target_os = "fuchsia"))]
+const UTSNAME_SIZE: usize = 65;
+#[cfg(any(target_os = "dragonfly", target_os = "haiku"))]
+const UTSNAME_SIZE: usize = 32;
+
+impl From<UtsName> for utsname {
+    fn from(uts_name: UtsName) -> Self {
+        let mut sysname = [0; UTSNAME_SIZE];
+        let mut nodename = [0; UTSNAME_SIZE];
+        let mut release = [0; UTSNAME_SIZE];
+        let mut version = [0; UTSNAME_SIZE];
+        let mut machine = [0; UTSNAME_SIZE];
+        #[cfg(any(target_os = "linux", target_os = "fuchsia"))]
+        let mut domainname = [0; UTSNAME_SIZE];
+
+        uts_name.sysname.iter().enumerate().for_each(|(i, c)| sysname[i] = *c as c_char);
+        uts_name.nodename.iter().enumerate().for_each(|(i, c)| nodename[i] = *c as c_char);
+        uts_name.release.iter().enumerate().for_each(|(i, c)| release[i] = *c as c_char);
+        uts_name.version.iter().enumerate().for_each(|(i, c)| version[i] = *c as c_char);
+        uts_name.machine.iter().enumerate().for_each(|(i, c)| machine[i] = *c as c_char);
+
+        #[cfg(any(target_os = "linux", target_os = "fuchsia"))]
+        uts_name.domainname.iter().enumerate().for_each(|(i, c)| domainname[i] = *c as c_char);
+
+        utsname {
+            sysname,
+            nodename,
+            release,
+            version,
+            machine,
+            #[cfg(any(target_os = "linux", target_os = "fuchsia"))]
+            domainname,
+        }
     }
 }
