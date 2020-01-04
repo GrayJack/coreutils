9774741f2c0f3a78360cfb7f72ee9a3b12229cd2
GrayJack <gr41.j4ck@gmail.com>
2020-01-03 23:57:48 -0300

    Logname: Update logname to use system fuction to get the username

--- logname/src/main.rs
+++ logname/src/main.rs
@@ -1,12 +1,16 @@
 use std::{env, process};
 
+use coreutils_core::login_name::user_login_name;
+
 use clap::{load_yaml, App, AppSettings::ColoredHelp};
 
 fn main() {
     let yaml = load_yaml!("logname.yml");
     let _matches = App::from_yaml(yaml).settings(&[ColoredHelp]).get_matches();
 
-    let login_name = if let Ok(name) = env::var("LOGNAME") {
+    let login_name = if let Some(name) = user_login_name() {
+        format!("{}", name)
+    } else if let Ok(name) = env::var("LOGNAME") {
         name
     } else {
         eprintln!("logname: No login name found.");
