0264d990c8f195698906c8721ceff3269d288172
GrayJack <gr41.j4ck@gmail.com>
2020-02-03 08:09:39 -0300

    Core: os: process: priority: Remove the Custom error type.  Now functions returns plain simple io::Error.

--- coreutils_core/src/os/process/priority.rs
+++ coreutils_core/src/os/process/priority.rs
@@ -1,9 +1,7 @@
 //! Module for safe API for getting and setting process priority
 
 use std::{
-    error::Error as StdError,
-    fmt::{self, Display},
-    io::Error as IOError,
+    io::{self, Error as IOError},
     os::raw::c_int,
 };
 
@@ -16,53 +14,27 @@ use libc::id_t;
 #[cfg(target_os = "linux")]
 use libc::c_uint;
 
-/// Possible errors
-#[derive(Debug)]
-pub enum Error {
-    SetPriority(IOError),
-    GetPriority(IOError),
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::SetPriority(ioerr) => write!(f, "setpriority: failed to set priority: {}", ioerr),
-            Self::GetPriority(ioerr) => write!(f, "getpriority: failed to get priority: {}", ioerr),
-        }
-    }
-}
-
-impl StdError for Error {
-    #[inline]
-    fn source(&self) -> Option<&(dyn StdError + 'static)> {
-        match self {
-            Self::SetPriority(ioerr) => Some(ioerr),
-            Self::GetPriority(ioerr) => Some(ioerr),
-        }
-    }
-}
-
 /// This function returns the highest priority (lowest numerical value) enjoyed by any of
 /// the specified processes if successful.
 #[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
-pub fn get_priority(which: c_int, who: c_int) -> Result<c_int, Error> {
+pub fn get_priority(which: c_int, who: c_int) -> io::Result<c_int> {
     let res = unsafe { getpriority(which, who) };
 
     match IOError::last_os_error().raw_os_error().unwrap() {
         0 => Ok(res),
-        _ => Err(Error::GetPriority(IOError::last_os_error())),
+        _ => Err(IOError::last_os_error()),
     }
 }
 
 /// This function returns the highest priority (lowest numerical value) enjoyed by any of
 /// the specified processes if successful.
 #[cfg(not(any(target_os = "freebsd", target_os = "dragonfly", target_os = "linux")))]
-pub fn get_priority(which: c_int, who: id_t) -> Result<c_int, Error> {
+pub fn get_priority(which: c_int, who: id_t) -> io::Result<c_int> {
     let res = unsafe { getpriority(which, who) };
 
     match IOError::last_os_error().raw_os_error().unwrap() {
         0 => Ok(res),
-        _ => Err(Error::GetPriority(IOError::last_os_error())),
+        _ => Err(IOError::last_os_error()),
     }
 }
 
@@ -72,31 +44,31 @@ pub fn get_priority(which: c_int, who: id_t) -> Result<c_int, Error> {
 pub fn get_priority(
     #[cfg(target_env = "musl")] which: c_int, #[cfg(not(target_env = "musl"))] which: c_uint,
     who: id_t,
-) -> Result<c_int, Error>
+) -> io::Result<c_int>
 {
     let res = unsafe { getpriority(which, who) };
 
     match IOError::last_os_error().raw_os_error().unwrap() {
         0 => Ok(res),
-        _ => Err(Error::GetPriority(IOError::last_os_error())),
+        _ => Err(IOError::last_os_error()),
     }
 }
 
 /// Set the priority of a specified process.
 #[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
-pub fn set_priority(which: c_int, who: c_int, prio: c_int) -> Result<(), Error> {
+pub fn set_priority(which: c_int, who: c_int, prio: c_int) -> io::Result<()> {
     match unsafe { setpriority(which, who, prio) } {
         0 => Ok(()),
-        _ => Err(Error::SetPriority(IOError::last_os_error())),
+        _ => Err(IOError::last_os_error()),
     }
 }
 
 /// Set the priority of a specified process.
 #[cfg(not(any(target_os = "freebsd", target_os = "dragonfly", target_os = "linux")))]
-pub fn set_priority(which: c_int, who: id_t, prio: c_int) -> Result<(), Error> {
+pub fn set_priority(which: c_int, who: id_t, prio: c_int) -> io::Result<()> {
     match unsafe { setpriority(which, who, prio) } {
         0 => Ok(()),
-        _ => Err(Error::SetPriority(IOError::last_os_error())),
+        _ => Err(IOError::last_os_error()),
     }
 }
 
@@ -105,10 +77,10 @@ pub fn set_priority(which: c_int, who: id_t, prio: c_int) -> Result<(), Error> {
 pub fn set_priority(
     #[cfg(target_env = "musl")] which: c_int, #[cfg(not(target_env = "musl"))] which: c_uint,
     who: id_t, prio: c_int,
-) -> Result<(), Error>
+) -> io::Result<()>
 {
     match unsafe { setpriority(which, who, prio) } {
         0 => Ok(()),
-        _ => Err(Error::SetPriority(IOError::last_os_error())),
+        _ => Err(IOError::last_os_error()),
     }
 }
