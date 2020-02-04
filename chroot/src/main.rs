8018d9e0083ed849203ad2ba3f2923dd907efd06
GrayJack <gr41.j4ck@gmail.com>
2020-02-04 04:39:28 -0300

    Chroot: Remove unwraps and expects and adapt to changes in the Core API  This fixes several cases of panics in the code

--- chroot/src/main.rs
+++ chroot/src/main.rs
@@ -1,9 +1,14 @@
-use std::io;
+use std::{
+    os::{raw::c_int, unix::process::CommandExt},
+    process::{self, Command},
+};
 
-use coreutils_core::os::{
-    group::{Group, Groups},
-    passwd::Passwd,
-    process::{change_root, set_group, set_groups, set_user},
+use coreutils_core::{
+    libc::ENOENT,
+    os::{
+        group::Error as GrError,
+        process::{change_root, set_group, set_groups, set_user},
+    },
 };
 
 use clap::{load_yaml, App, AppSettings::ColoredHelp};
@@ -12,6 +17,7 @@ fn main() {
     let yaml = load_yaml!("chroot.yml");
     let matches = App::from_yaml(yaml).settings(&[ColoredHelp]).get_matches();
 
+    // Ok to unwrap cause it's required argument
     let root = matches.value_of("NEWROOT").unwrap();
     let cmd = matches.value_of("COMMAND").unwrap_or("/bin/sh");
     let args: Vec<&str> = match matches.values_of("ARGS") {
@@ -19,42 +25,56 @@ fn main() {
         None => vec!["-i"],
     };
 
-    change_root(root).map_err(|e| eprintln!("error changing root {:?}", e)).unwrap();
+    if let Err(err) = change_root(root) {
+        eprintln!("chroot: Cannot change root directory to {}: {}", root, err);
+        process::exit(125);
+    }
 
     if let Some(groups_list) = matches.value_of("groups") {
-        set_groups_from_list(groups_list)
-            .expect("unable to set groups for process from group list");
+        if let Err(err) = set_groups_from_list(groups_list) {
+            eprintln!("chroot: Unable to set a group from supplementary list: {}", err);
+            process::exit(1);
+        }
     }
 
     if let Some(userspec_str) = matches.value_of("userspec") {
-        set_user_from_userspec(userspec_str)
-            .expect("unable to set groups for process from userspec");
+        if let Err(err) = set_user_from_userspec(userspec_str) {
+            eprintln!("chroot: Unable to set user and/or group from userspec: {}", err);
+            process::exit(1);
+        }
     }
 
     if let Some(group) = matches.value_of("group") {
-        let group = Group::from_name(group).expect("unable to get group information");
-        set_group(group).expect("unable to set group for process");
+        if let Err(err) = set_group(group) {
+            eprintln!("chroot: Unable to set group for process: {}", err);
+            process::exit(1);
+        }
     }
 
     if let Some(user) = matches.value_of("user") {
-        let user = Passwd::from_name(user).expect("unable to get user from passwd");
-        set_user(user).expect("unable to set user for process");
+        if let Err(err) = set_user(user) {
+            eprintln!("chroot: Unable to set user for process: {}", err);
+            process::exit(1);
+        }
     }
 
-    let proc = std::process::Command::new(cmd).args(args).status().unwrap();
+    let err = Command::new(cmd).args(args).exec();
 
-    let exit_code = if proc.success() { 0 } else { proc.code().unwrap_or(1) };
-
-    std::process::exit(exit_code);
+    if err.raw_os_error().unwrap() as c_int == ENOENT {
+        eprintln!("chroot: '{}': {}", cmd, err);
+        process::exit(127);
+    } else {
+        eprintln!("chroot: {}", err);
+        process::exit(126);
+    }
 }
 
-fn set_groups_from_list(groups_list: &str) -> io::Result<()> {
+fn set_groups_from_list(groups_list: &str) -> Result<(), GrError> {
     let groups: Vec<&str> = groups_list.split(',').collect();
-    let groups = Groups::from_group_list(&groups).expect("unable to get groups from group list");
-    set_groups(groups)
+    Ok(set_groups(&groups)?)
 }
 
-fn set_user_from_userspec(userspec: &str) -> io::Result<()> {
+fn set_user_from_userspec(userspec: &str) -> Result<(), GrError> {
     let parts: Vec<&str> = userspec.split(':').collect();
     if parts.len() != 2 {
         eprintln!("userspec is in an incorrect format");
@@ -62,8 +82,6 @@ fn set_user_from_userspec(userspec: &str) -> io::Result<()> {
     }
 
     let (user, group) = (parts[0], parts[1]);
-    let user = Passwd::from_name(user).expect("unable to get user from name");
-    let group = Group::from_name(group).expect("unable to get group from name");
 
     set_group(group)?;
     set_user(user)?;
