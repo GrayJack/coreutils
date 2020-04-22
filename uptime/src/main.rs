c6efd45ac62d4af4e052f64b6bd07be40d36a4b5
GrayJack <gr41.j4ck@gmail.com>
2020-04-22 01:49:58 -0300

    Uptime: Fix compilation on 32bit target

--- uptime/src/main.rs
+++ uptime/src/main.rs
@@ -94,7 +94,7 @@ fn main() {
 
 fn uptime(boot_time: DateTime) -> Result<i64, ostime::Error> {
     match ostime::uptime() {
-        Ok(t) => Ok(t.tv_sec),
+        Ok(t) => Ok(t.tv_sec as i64),
         Err(ostime::Error::TargetNotSupported) => Ok((DateTime::now() - boot_time).whole_seconds()),
         Err(err) => Err(err),
     }
@@ -134,7 +134,7 @@ fn fmt_number_users(num_users: usize) -> String {
     }
 }
 
-fn fmt_uptime(upsecs: time_t, pretty_flag: bool) -> String {
+fn fmt_uptime(upsecs: i64, pretty_flag: bool) -> String {
     let updays = upsecs / 86400;
     let uphours = (upsecs - (updays * 86400)) / 3600;
     let upmins = (upsecs - (updays * 86400) - (uphours * 3600)) / 60;
