a7f4acd391149d5ed648fe806ecb188de164d912
GrayJack <gr41.j4ck@gmail.com>
2020-05-05 18:51:12 -0300

    Pwd: Remove YAML file

--- pwd/src/main.rs
+++ pwd/src/main.rs
@@ -2,11 +2,10 @@ use std::process;
 
 use coreutils_core::env;
 
-use clap::{load_yaml, App, AppSettings::ColoredHelp};
+mod cli;
 
 fn main() {
-    let yaml = load_yaml!("pwd.yml");
-    let matches = App::from_yaml(yaml).settings(&[ColoredHelp]).get_matches();
+    let matches = cli::create_app().get_matches();
 
     let curr_dir = {
         // The local path we get from environment variable PWD
@@ -22,7 +21,7 @@ fn main() {
                 }
             },
             Err(e) => {
-                eprintln!("pwd: Failed to get current directory. {:#?}", e);
+                eprintln!("pwd: Failed to get current directory. {}", e);
                 process::exit(1);
             },
         }
