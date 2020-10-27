650c6349288302567399f80100dbe836b11aaeeb
GrayJack <gr41.j4ck@gmail.com>
2020-10-26 22:41:49 -0300

    Sort: Some text changes

--- sort/src/cli.rs
+++ sort/src/cli.rs
@@ -13,26 +13,26 @@ pub(crate) fn create_app<'a, 'b>() -> App<'a, 'b> {
         .settings(&[ColoredHelp])
         .arg(
             Arg::with_name("FILE")
-                .help("The file( operands are processed in command-line order.")
+                .help("File(s) to be sorted, merged or checked.")
                 .long_help(
-                    "The file operands are processed in command-line order.\n\nIf file is a \
-                     single dash (‘-’) or absent, sort reads from the standard input.",
+                    "File(s) to be sorted, merged or checked.\n\nIf FILE is ‘-’ or absent, sort \
+                     reads from the standard input.",
                 )
                 .multiple(true),
         )
         .arg(
             Arg::with_name("merge_only")
-                .help("Merge only")
-                .long_help("Merge only; the input file shall be assumed to be already sorted.")
+                .help("Merge files.")
+                .long_help("Merge files\n\nThe input FILE(s) are assumed to be already sorted.")
                 .short("m"),
         )
         .arg(
             Arg::with_name("output")
-                .help("Output file")
                 .value_name("FILE")
+                .help("Write result to FILE instead of standard output.")
                 .long_help(
-                    "Specify the name of an output file to be used instead of the standard \
-                     output. This file can be the same as one of the input files.",
+                    "Write result to FILE instead of standard output.\n\nThis FILE can be the \
+                     same one as one of the input FILE(s)",
                 )
                 .short("o"),
         )
