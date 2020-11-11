9100a17760c626abdff1276e55c3e0d952e967ed
GrayJack <gr41.j4ck@gmail.com>
2020-11-10 22:22:10 -0300

    Yes: Simplify

--- yes/src/main.rs
+++ yes/src/main.rs
@@ -5,8 +5,7 @@ mod cli;
 fn main() {
     let matches = cli::create_app().get_matches();
 
-    let string = if matches.is_present("STRING") {
-        let inputs = matches.values_of("STRING").unwrap();
+    let string = if let Some(inputs) = matches.values_of("STRING") {
         inputs.fold(String::new(), |res, s| res + s + " ")
     } else {
         "y".to_string()
