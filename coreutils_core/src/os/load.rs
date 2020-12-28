21a05245c9dd8621052ea937bd640b9844964dc1
GrayJack <gr41.j4ck@gmail.com>
2020-12-28 03:10:10 -0300

    Core: os: load: Change to return Option
    `getloadavg` doesn't set errno on fail, so it doesn't make sense to return io::Result

--- coreutils_core/src/os/load.rs
+++ coreutils_core/src/os/load.rs
@@ -1,20 +1,17 @@
 //! Module related to system load.
-use std::{io, os::raw::c_double};
+use std::os::raw::c_double;
 
 use libc::getloadavg;
 
-/// Returns 3 load average from the system.
+/// Returns 3 load averages from the system.
 ///
 /// These 3 loads represent the averages over the last 1, 5 and 15 minutes, respectively.
-///
-/// # Errors
-/// If a internal call set a errno (I/O OS error), an error variant will be returned.
 #[inline]
-pub fn load_average() -> io::Result<[c_double; 3]> {
+pub fn load_average() -> Option<[c_double; 3]> {
     let mut avg: [c_double; 3] = [0.0; 3];
 
     match unsafe { getloadavg(avg.as_mut_ptr(), 3) } {
-        -1 => Err(io::Error::last_os_error()),
-        _ => Ok(avg),
+        -1 => None,
+        _ => Some(avg),
     }
 }
