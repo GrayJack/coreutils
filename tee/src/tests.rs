172024f98efc3828a9ac3bf5f35b1ce331e3ae02
Jeremy Jackson <git@jeremyvii.com>
2021-01-05 14:04:23 -0600

    Tee: test with named temp file

--- tee/src/tests.rs
+++ tee/src/tests.rs
@@ -1,9 +1,7 @@
 use super::*;
 use assert_cmd::Command;
-use std::{
-    error::Error,
-    fs::{remove_file, File, OpenOptions},
-};
+use std::{error::Error, fs::File};
+use tempfile::NamedTempFile;
 
 #[test]
 fn tee_copy_buffer() {
@@ -28,18 +26,16 @@ fn tee_copy_stdin_to_stdout() -> Result<(), Box<dyn Error>> {
 #[test]
 fn tee_copy_stdin_to_file() -> Result<(), Box<dyn Error>> {
     let buffer = "Hello World!";
-    let test_file = "test_file";
+    let temp_file = NamedTempFile::new()?;
 
     let mut cmd = Command::cargo_bin("tee")?;
-    cmd.arg("-a").arg("test_file").write_stdin(buffer).output()?;
+    cmd.arg("-a").arg(temp_file.path()).write_stdin(buffer).output()?;
 
-    let mut file = File::open(test_file)?;
+    let mut file = File::open(temp_file)?;
     let mut file_buffer = String::new();
     file.read_to_string(&mut file_buffer)?;
 
     assert_eq!(buffer.to_owned(), file_buffer);
-
-    remove_file(test_file)?;
 
     Ok(())
 }
@@ -47,21 +43,18 @@ fn tee_copy_stdin_to_file() -> Result<(), Box<dyn Error>> {
 #[test]
 fn tee_append_stdin_to_file() -> Result<(), Box<dyn Error>> {
     let buffer = "Hello World!";
-    let test_file = "test_file";
+    let mut temp_file = NamedTempFile::new()?;
 
-    let mut file = OpenOptions::new().read(true).write(true).create(true).open(test_file)?;
-    file.write_all(b"Test\n")?;
+    temp_file.write_all(b"Test\n")?;
 
     let mut cmd = Command::cargo_bin("tee")?;
-    cmd.arg("-a").arg("test_file").write_stdin(buffer).output()?;
+    cmd.arg("-a").arg(temp_file.path()).write_stdin(buffer).output()?;
 
-    let mut file = File::open(test_file)?;
+    let mut file = File::open(temp_file)?;
     let mut file_buffer = String::from("Test\n");
     file.read_to_string(&mut file_buffer)?;
 
     assert_eq!("Test\nHello World!".to_owned(), file_buffer);
-
-    remove_file(test_file)?;
 
     Ok(())
 }
