b9ad3d59145816fcd01a6c1c67766846b0e72fc6
Vaibhav Yenamandra <3663231+envp@users.noreply.github.com>
2021-02-27 21:25:16 -0500

    Time: cli: Add -p flag, and accept a command to run

--- time/src/cli.rs
+++ time/src/cli.rs
@@ -1,5 +1,5 @@
 use clap::{
-    crate_authors, crate_description, crate_name, crate_version, App, AppSettings::ColoredHelp,
+    crate_authors, crate_description, crate_name, crate_version, App, AppSettings::ColoredHelp, Arg,
 };
 
 pub(crate) fn create_app<'a, 'b>() -> App<'a, 'b> {
@@ -11,5 +11,57 @@ pub(crate) fn create_app<'a, 'b>() -> App<'a, 'b> {
         .version_message("Display version information.")
         .help_short("?")
         .settings(&[ColoredHelp]);
-    app
+
+    let posix_fmt = Arg::with_name("posix")
+        .help(
+            "Display time output in POSIX specified format as:\n\treal %f\n\tuser %f\n\tsys  \
+             %f\nTimer accuracy is arbitrary, but will always be counted in seconds.",
+        )
+        .short("p")
+        .long("--posix")
+        .takes_value(false);
+
+    let command = Arg::with_name("COMMAND").help("Command or utility to run.").required(true);
+
+    let arguments = Arg::with_name("ARGUMENT")
+        .help("Optional arguments to pass to <COMMAND>.")
+        .multiple(true)
+        .number_of_values(1)
+        .empty_values(false);
+
+    app.arg(posix_fmt).arg(command).arg(arguments)
+}
+
+#[derive(Debug)]
+pub struct Args {
+    pub output_fmt: OutputFormat,
+    pub executable: String,
+    pub exec_args:  Vec<String>,
+}
+
+impl Args {
+    pub fn new() -> Args {
+        let args = create_app().get_matches();
+        let command =
+            args.value_of("COMMAND").expect("`COMMAND` value cannot be `None`, it is required.");
+
+        Args {
+            output_fmt: if args.is_present("posix") {
+                OutputFormat::Posix
+            } else {
+                OutputFormat::Default
+            },
+            executable: command.to_owned(),
+            exec_args:  match args.values_of("arguments") {
+                Some(vs) => vs.into_iter().map(|item| item.to_owned()).collect(),
+                None => vec![],
+            },
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum OutputFormat {
+    Default,
+    Posix,
 }
