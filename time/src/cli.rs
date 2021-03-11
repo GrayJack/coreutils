587ee7ba616ef6a4dfdb5c5db6fb209fe04d5e88
GrayJack <gr41.j4ck@gmail.com>
2021-03-11 19:25:54 -0300

    Time: Add long name to -p

--- time/src/cli.rs
+++ time/src/cli.rs
@@ -3,27 +3,29 @@ use clap::{
 };
 
 pub(crate) fn create_app<'a, 'b>() -> App<'a, 'b> {
-    let app = App::new(crate_name!())
+    App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
         .help_message("Display help information.")
         .version_message("Display version information.")
         .help_short("?")
-        .settings(&[ColoredHelp]);
-
-    let posix_fmt = Arg::with_name("posix")
-        .help(
-            "Display time output in POSIX specified format as:\n\treal %f\n\tuser %f\n\tsys  \
-             %f\nTimer accuracy is arbitrary, but will always be counted in seconds.",
+        .settings(&[ColoredHelp])
+        .arg(Arg::with_name("COMMAND").help("Command or utility to run.").required(true))
+        .arg(
+            Arg::with_name("ARGUMENT")
+                .help("Optional arguments to pass to <COMMAND>.")
+                .multiple(true),
         )
-        .short("p")
-        .takes_value(false);
-
-    let command = Arg::with_name("COMMAND").help("Command or utility to run.").required(true);
-
-    let arguments =
-        Arg::with_name("ARGUMENT").help("Optional arguments to pass to <COMMAND>.").multiple(true);
-
-    app.args(&[posix_fmt, command, arguments])
+        .arg(
+            Arg::with_name("posix")
+                .help(
+                    "Display time output in POSIX specified format as:\n\treal %f\n\tuser \
+                     %f\n\tsys  %f\nTimer accuracy is arbitrary, but will always be counted in \
+                     seconds.",
+                )
+                .long("posix")
+                .short("p")
+                .takes_value(false),
+        )
 }
