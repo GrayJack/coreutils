202f0d24f31745b72f15f7b0aabea7367bfed578
GrayJack <gr41.j4ck@gmail.com>
2022-01-02 05:53:04 -0300

    Pwd: Update to clap 3.0.0

--- pwd/src/cli.rs
+++ pwd/src/cli.rs
@@ -1,36 +1,32 @@
-use clap::{
-    crate_authors, crate_description, crate_name, crate_version, App, AppSettings::ColoredHelp, Arg,
-};
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
 
-pub(crate) fn create_app<'a, 'b>() -> App<'a, 'b> {
+pub(crate) fn create_app<'help>() -> App<'help> {
     App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
-        .help_message("Display help information.")
-        .version_message("Display version information.")
-        .help_short("?")
-        .settings(&[ColoredHelp])
+        .mut_arg("help", |help| help.help("Display help information.").short('?'))
+        .mut_arg("version", |v| v.help("Display version information."))
         .arg(
-            Arg::with_name("logical")
+            Arg::new("logical")
                 .help("Display the logical current working directory.")
                 .long_help(
                     "Display the logical current working directory.\n\nUse PWD from environment, \
                      even if it contains symlinks.",
                 )
                 .long("logical")
-                .short("l")
+                .short('l')
                 .overrides_with("physical"),
         )
         .arg(
-            Arg::with_name("physical")
+            Arg::new("physical")
                 .help("Display the physical current working directory. Avoid all symlinks.")
                 .long_help(
                     "Display the physical current working directory. Avoid all symlinks (All \
                      symbolic links resolved).",
                 )
                 .long("physical")
-                .short("p")
+                .short('p')
                 .overrides_with("logical"),
         )
 }
