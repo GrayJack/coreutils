dda7be0ead2dbd3b788e65cca933ba27d8080a8b
GrayJack <gr41.j4ck@gmail.com>
2022-01-02 05:50:28 -0300

    Clear: Update to clap 3.0.0

--- clear/src/cli.rs
+++ clear/src/cli.rs
@@ -1,20 +1,11 @@
-use clap::{
-    crate_authors, crate_description, crate_name, crate_version, App, AppSettings::ColoredHelp, Arg,
-};
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
 
-pub(crate) fn create_app<'a, 'b>() -> App<'a, 'b> {
+pub(crate) fn create_app<'help>() -> App<'help> {
     App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
-        .help_message("Display help information.")
-        .version_message("Display version information.")
-        .help_short("?")
-        .settings(&[ColoredHelp])
-        .arg(
-            Arg::with_name("x")
-                .help("Do not try to clear scrollback.")
-                .long("no-scrollback")
-                .short("x"),
-        )
+        .mut_arg("help", |help| help.help("Display help information.").short('?'))
+        .mut_arg("version", |v| v.help("Display version information."))
+        .arg(Arg::new("x").help("Do not try to clear scrollback.").long("no-scrollback").short('x'))
 }
