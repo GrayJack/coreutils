91426ead53e6cba13ff060cbed2b47bd980a7f60
GrayJack <gr41.j4ck@gmail.com>
2022-01-02 05:50:28 -0300

    Link: Update to clap 3.0.0

--- link/src/cli.rs
+++ link/src/cli.rs
@@ -1,16 +1,12 @@
-use clap::{
-    crate_authors, crate_description, crate_name, crate_version, App, AppSettings::ColoredHelp, Arg,
-};
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
 
-pub(crate) fn create_app<'a, 'b>() -> App<'a, 'b> {
+pub(crate) fn create_app<'help>() -> App<'help> {
     App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
-        .help_message("Display help information.")
-        .version_message("Display version information.")
-        .help_short("?")
-        .settings(&[ColoredHelp])
-        .arg(Arg::with_name("FILE1").help("Existing file to be linked.").required(true))
-        .arg(Arg::with_name("FILE2").help("File name of the link to be created.").required(true))
+        .mut_arg("help", |help| help.help("Display help information.").short('?'))
+        .mut_arg("version", |v| v.help("Display version information."))
+        .arg(Arg::new("FILE1").help("Existing file to be linked.").required(true))
+        .arg(Arg::new("FILE2").help("File name of the link to be created.").required(true))
 }
