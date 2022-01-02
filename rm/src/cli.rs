7bed8c5e61a799188290663357e6ea6f80fbe3ee
GrayJack <gr41.j4ck@gmail.com>
2022-01-02 05:53:04 -0300

    Rm: Update to clap 3.0.0

--- rm/src/cli.rs
+++ rm/src/cli.rs
@@ -1,28 +1,24 @@
-use clap::{
-    crate_authors, crate_description, crate_name, crate_version, App, AppSettings::ColoredHelp, Arg,
-};
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
 
-pub(crate) fn create_app<'a, 'b>() -> App<'a, 'b> {
+pub(crate) fn create_app<'help>() -> App<'help> {
     App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
-        .help_message("Display help information.")
-        .version_message("Display version information.")
-        .help_short("?")
-        .settings(&[ColoredHelp])
+        .mut_arg("help", |help| help.help("Display help information.").short('?'))
+        .mut_arg("version", |v| v.help("Display version information."))
         .arg(
-            Arg::with_name("FILE")
+            Arg::new("FILE")
                 .help("File(s) to be deleted.")
                 .long_help(
                     "File(s) to be deleted.\n\nTo remove a file whose name starts with a '-', for \
                      example '-foo', use 'rm -- -foo'",
                 )
                 .required(true)
-                .multiple(true),
+                .multiple_occurrences(true),
         )
         .arg(
-            Arg::with_name("force")
+            Arg::new("force")
                 .help(
                     "Ignore nonexistent files, never prompt regardless of the file's permissions.",
                 )
@@ -31,21 +27,21 @@ pub(crate) fn create_app<'a, 'b>() -> App<'a, 'b> {
                      permissions.\n\nThis option overrides any previous -i or -I.",
                 )
                 .long("force")
-                .short("f")
+                .short('f')
                 .overrides_with_all(&["interactive", "interactiveBatch"]),
         )
         .arg(
-            Arg::with_name("interactive")
+            Arg::new("interactive")
                 .help("Prompt before every removal.")
                 .long_help(
                     "Prompt before every removal.\n\nThis option overrides any previous -f or -I.",
                 )
                 .long("interactive")
-                .short("i")
+                .short('i')
                 .overrides_with_all(&["force", "interactiveBatch"]),
         )
         .arg(
-            Arg::with_name("interactiveBatch")
+            Arg::new("interactiveBatch")
                 .help(
                     "Prompt once before removing more than three files, or when removing \
                      recursively.",
@@ -56,25 +52,25 @@ pub(crate) fn create_app<'a, 'b>() -> App<'a, 'b> {
                      against most mistakes.\n\nThis option overrides any previous -f or -i.",
                 )
                 .long("interactive-batch")
-                .short("I")
+                .short('I')
                 .overrides_with_all(&["force", "interactive"]),
         )
         .arg(
-            Arg::with_name("noPreserveRoot")
+            Arg::new("noPreserveRoot")
                 .help("Do not treat '/' specially.")
                 .long("no-preserve-root")
-                .short("n")
+                .short('n')
                 .conflicts_with("preserveRoot"),
         )
         .arg(
-            Arg::with_name("preserveRoot")
+            Arg::new("preserveRoot")
                 .help("Do not remove '/'. (default)")
                 .long("preserve-root")
-                .short("p")
+                .short('p')
                 .conflicts_with("noPreserveRoot"),
         )
         .arg(
-            Arg::with_name("recursive")
+            Arg::new("recursive")
                 .help("Remove directories and their contents recursively.")
                 .long_help(
                     "Remove directories and their contents recursively.\n\nThis option implies \
@@ -85,10 +81,10 @@ pub(crate) fn create_app<'a, 'b>() -> App<'a, 'b> {
                      skipped.",
                 )
                 .long("recursive")
-                .short("r"),
+                .short('r'),
         )
         .arg(
-            Arg::with_name("recursive_compat")
+            Arg::new("recursive_compat")
                 .help("Remove directories and their contents recursively.")
                 .long_help(
                     "Remove directories and their contents recursively.\n\nThis option implies \
@@ -99,13 +95,8 @@ pub(crate) fn create_app<'a, 'b>() -> App<'a, 'b> {
                      skipped.",
                 )
                 .long("recursive-compat")
-                .short("R"),
+                .short('R'),
         )
-        .arg(Arg::with_name("directories").help("Remove empty directories.").long("dir").short("d"))
-        .arg(
-            Arg::with_name("verbose")
-                .help("Explain what is being done.")
-                .long("verbose")
-                .short("v"),
-        )
+        .arg(Arg::new("directories").help("Remove empty directories.").long("dir").short('d'))
+        .arg(Arg::new("verbose").help("Explain what is being done.").long("verbose").short('v'))
 }
