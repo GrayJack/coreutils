3933b33c726644ce553b6b56879bd3c9e005edbb
GrayJack <gr41.j4ck@gmail.com>
2022-01-02 05:53:04 -0300

    Unexpand: Update to clap 3.0.0

--- unexpand/src/cli.rs
+++ unexpand/src/cli.rs
@@ -1,39 +1,37 @@
-use clap::{
-    crate_authors, crate_description, crate_name, crate_version, App, AppSettings::ColoredHelp, Arg,
-};
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
 
-pub(crate) fn create_app<'a, 'b>() -> App<'a, 'b> {
+pub(crate) fn create_app<'help>() -> App<'help> {
     App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
-        .help_message("Display help information.")
-        .version_message("Display version information.")
-        .help_short("?")
-        .settings(&[ColoredHelp])
+        .mut_arg("help", |help| help.help("Display help information.").short('?'))
+        .mut_arg("version", |v| v.help("Display version information."))
         .arg(
-            Arg::with_name("FILE").help("File(s) to use, or '-' to use from stdin.").multiple(true),
+            Arg::new("FILE")
+                .help("File(s) to use, or '-' to use from stdin.")
+                .multiple_occurrences(true),
         )
         .arg(
-            Arg::with_name("all")
+            Arg::new("all")
                 .help("Convert all blanks, instead of just initial blanks.")
                 .long_help(
                     "Convert all blanks, instead of just initial blanks.\n\nThis flag overrides -f",
                 )
                 .long("all")
-                .short("a")
+                .short('a')
                 .overrides_with("first_only"),
         )
         .arg(
-            Arg::with_name("first_only")
+            Arg::new("first_only")
                 .help("Convert only leading sequences of blanks.")
                 .long_help("Convert only leading sequences of blanks.\n\nThis flag overrides -a")
                 .long("first-only")
-                .short("f")
+                .short('f')
                 .overrides_with("all"),
         )
         .arg(
-            Arg::with_name("tabs")
+            Arg::new("tabs")
                 .help(
                     "Have tabs N characters apart instead of 8 OR Comma separated LIST of tab \
                      positions.",
@@ -47,7 +45,7 @@ pub(crate) fn create_app<'a, 'b>() -> App<'a, 'b> {
                      first column.\n\nThis options implies -a flag.",
                 )
                 .long("tabs")
-                .short("t")
+                .short('t')
                 .value_name("N or LIST"),
         )
 }
