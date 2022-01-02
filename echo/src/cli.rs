059b579e347c882f68821c5a3bed9ee46fb59b22
GrayJack <gr41.j4ck@gmail.com>
2022-01-02 05:50:28 -0300

    Echo: Update to clap 3.0.0

--- echo/src/cli.rs
+++ echo/src/cli.rs
@@ -1,33 +1,29 @@
-use clap::{
-    crate_authors, crate_description, crate_name, crate_version, App, AppSettings::ColoredHelp, Arg,
-};
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
 
-pub(crate) fn create_app<'a, 'b>() -> App<'a, 'b> {
+pub(crate) fn create_app<'help>() -> App<'help> {
     App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
-        .help_message("Display help information.")
-        .version_message("Display version information.")
-        .help_short("?")
-        .settings(&[ColoredHelp])
+        .mut_arg("help", |help| help.help("Display help information.").short('?'))
+        .mut_arg("version", |v| v.help("Display version information."))
         .arg(
-            Arg::with_name("STRING")
+            Arg::new("STRING")
                 .help("The text to be displayed.")
                 .required(true)
-                .multiple(true),
+                .multiple_occurrences(true),
         )
         .arg(
-            Arg::with_name("escape")
+            Arg::new("escape")
                 .help("Enable interpretation of backslash escapes.")
                 .long("escapes")
                 .visible_alias("backslash-escapes")
-                .short("e"),
+                .short('e'),
         )
         .arg(
-            Arg::with_name("no_newline")
+            Arg::new("no_newline")
                 .help("Do not output the trailing newline.")
                 .long("no-newline")
-                .short("n"),
+                .short('n'),
         )
 }
