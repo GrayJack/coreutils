0c7cc8d52c347356f5982e24c1419d5b55bf667d
GrayJack <gr41.j4ck@gmail.com>
2022-01-02 05:50:28 -0300

    Cat: Update to clap 3.0.0

--- cat/build.rs
+++ cat/build.rs
@@ -1,12 +1,14 @@
-use std::env;
+use std::{env, fs::File, io::BufWriter};
 
-use clap::Shell;
+use clap::crate_name;
+use clap_generate::{Generator, Shell};
 
 #[path = "src/cli.rs"]
 mod cli;
 
 fn main() {
     let mut app = cli::create_app();
+    app.set_bin_name(crate_name!());
 
     let out_dir = match env::var("OUT_DIR") {
         Ok(dir) => dir,
@@ -16,9 +18,19 @@ fn main() {
         },
     };
 
-    app.gen_completions("cat", Shell::Zsh, out_dir.clone());
-    app.gen_completions("cat", Shell::Fish, out_dir.clone());
-    app.gen_completions("cat", Shell::Bash, out_dir.clone());
-    app.gen_completions("cat", Shell::PowerShell, out_dir.clone());
-    app.gen_completions("cat", Shell::Elvish, out_dir);
+    let shells = [Shell::Bash, Shell::Elvish, Shell::Fish, Shell::PowerShell, Shell::Zsh];
+
+    for shell in shells {
+        let file_name = format!("{}/{}", out_dir, shell.file_name(app.get_name()));
+        let mut file = BufWriter::new(
+            File::options()
+                .read(true)
+                .write(true)
+                .create(true)
+                .open(file_name)
+                .expect("Unable to open file"),
+        );
+
+        shell.generate(&app, &mut file)
+    }
 }
