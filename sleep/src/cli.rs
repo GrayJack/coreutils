2d2878a0da4a3bd2af0f8b6d84c65deec4d05470
GrayJack <gr41.j4ck@gmail.com>
2022-01-02 05:53:04 -0300

    Sleep: Update to clap 3.0.0

--- sleep/src/cli.rs
+++ sleep/src/cli.rs
@@ -1,29 +1,25 @@
-use clap::{
-    crate_authors, crate_description, crate_name, crate_version, App, AppSettings::ColoredHelp, Arg,
-};
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
 
-pub(crate) fn create_app<'a, 'b>() -> App<'a, 'b> {
+pub(crate) fn create_app<'help>() -> App<'help> {
     App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
-        .help_message("Display help information.")
-        .version_message("Display version information.")
-        .help_short("?")
-        .settings(&[ColoredHelp])
+        .mut_arg("help", |help| help.help("Display help information.").short('?'))
+        .mut_arg("version", |v| v.help("Display version information."))
         .arg(
-            Arg::with_name("NUMBER")
+            Arg::new("NUMBER")
                 .help(
                     "Number to set the sleep. If more than one, will sleep the sum of the numbers.",
                 )
                 .required(true)
-                .multiple(true),
+                .multiple_occurrences(true),
         )
         .arg(
-            Arg::with_name("suffix")
+            Arg::new("suffix")
                 .help("A optional parameter to set the time measurement to be used.")
                 .long("suffix")
-                .short("s")
+                .short('s')
                 .value_name("SUFFIX")
                 .default_value("sec"),
         )
