599af6e4156e0dc6ea896fc96a89d061415fd936
GrayJack <gr41.j4ck@gmail.com>
2022-01-02 05:52:34 -0300

    Nohup: Update to clap 3.0.0

--- nohup/src/cli.rs
+++ nohup/src/cli.rs
@@ -1,25 +1,23 @@
 use clap::{
-    crate_authors, crate_description, crate_name, crate_version, App,
-    AppSettings::{ColoredHelp, TrailingVarArg},
+    crate_authors, crate_description, crate_name, crate_version, App, AppSettings::TrailingVarArg,
     Arg,
 };
 
-pub(crate) fn create_app<'a, 'b>() -> App<'a, 'b> {
+pub(crate) fn create_app<'help>() -> App<'help> {
     App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
-        .help_message("Display help information.")
-        .version_message("Display version information.")
-        .help_short("?")
-        .settings(&[ColoredHelp, TrailingVarArg])
+        .mut_arg("help", |help| help.help("Display help information.").short('?'))
+        .mut_arg("version", |v| v.help("Display version information."))
+        .setting(TrailingVarArg)
         .arg(
-            Arg::with_name("COMMAND")
+            Arg::new("COMMAND")
                 .help(
                     "Command and arguments to be run which will ignore hangup signals and it's \
                      arguments.",
                 )
                 .required(true)
-                .multiple(true),
+                .multiple_occurrences(true),
         )
 }
