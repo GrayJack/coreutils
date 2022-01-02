ddee661104668f3adb10b75c032037419dcc19c9
GrayJack <gr41.j4ck@gmail.com>
2022-01-02 05:50:28 -0300

    Id: Update to clap 3.0.0

--- id/src/cli.rs
+++ id/src/cli.rs
@@ -1,109 +1,105 @@
-use clap::{
-    crate_authors, crate_description, crate_name, crate_version, App, AppSettings::ColoredHelp, Arg,
-};
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
 
 #[allow(clippy::let_and_return)]
-pub(crate) fn create_app<'a, 'b>() -> App<'a, 'b> {
+pub(crate) fn create_app<'help>() -> App<'help> {
     let app = App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
-        .help_message("Display help information.")
-        .version_message("Display version information.")
-        .help_short("?")
-        .settings(&[ColoredHelp])
+        .mut_arg("help", |help| help.help("Display help information.").short('?'))
+        .mut_arg("version", |v| v.help("Display version information."))
         .arg(
-            Arg::with_name("USER")
+            Arg::new("USER")
                 .help("Name of a user.")
                 .long_help("Name of a user.\n\nIf not specified assumes the current user"),
         )
         .arg(
-            Arg::with_name("group")
+            Arg::new("group")
                 .help("Display only the effective group ID as number.")
                 .long("group")
-                .short("g")
+                .short('g')
                 .conflicts_with_all(&["groups", "user", "pretty", "human", "file"]),
         )
         .arg(
-            Arg::with_name("groups")
+            Arg::new("groups")
                 .help("Display all group IDs.")
                 .long("groups")
-                .short("G")
+                .short('G')
                 .conflicts_with_all(&["group", "user", "pretty", "human", "file"]),
         )
         .arg(
-            Arg::with_name("user")
+            Arg::new("user")
                 .help("Display only the effective user ID as number.")
                 .long("user")
-                .short("u")
+                .short('u')
                 .conflicts_with_all(&["group", "groups", "pretty", "human", "file"]),
         )
         .arg(
-            Arg::with_name("name")
+            Arg::new("name")
                 .help(
                     "Display the name of the user or group ID for the -G, -g and -u options \
                      instead of the number.",
                 )
                 .long("name")
-                .short("n")
+                .short('n')
                 .conflicts_with_all(&["pretty", "human", "file"]),
         )
         .arg(
-            Arg::with_name("real")
+            Arg::new("real")
                 .help("Display the real ID for the -g and -u options instead of the effective ID.")
                 .long("real")
-                .short("r")
+                .short('r')
                 .conflicts_with_all(&["pretty", "human", "file", "groups"]),
         )
         .arg(
-            Arg::with_name("zero")
+            Arg::new("zero")
                 .help("Delimit entries with NULL characters, not whitespace.")
                 .long_help(
                     "Delimit entries with NULL characters, not whitespace.\n\nNot permitted in \
                      default format.",
                 )
                 .long("zero")
-                .short("z"),
+                .short('z'),
         )
         .arg(
-            Arg::with_name("pretty")
+            Arg::new("pretty")
                 .help("Make the output human-readable.")
                 .long("pretty")
-                .short("p")
+                .short('p')
                 .conflicts_with_all(&["group", "groups", "user", "name", "file"]),
         )
         .arg(
-            Arg::with_name("human")
+            Arg::new("human")
                 .help("Make the output human-readable.")
                 .long("human-readable")
-                .short("h")
+                .short('h')
                 .conflicts_with_all(&["group", "groups", "user", "name", "file"]),
         )
         .arg(
-            Arg::with_name("file")
+            Arg::new("file")
                 .help("Display the id as a password file entry.")
                 .long("file")
-                .short("P")
+                .short('P')
                 .conflicts_with_all(&["group", "groups", "user", "name", "pretty", "human"]),
         );
 
     #[cfg(any(target_os = "freebsd", target_os = "macos"))]
     let app = app.arg(
-        Arg::with_name("audit")
+        Arg::new("audit")
             .help(
                 "Display the process audit user ID and other process audit properties, which \
                  requires privilege.",
             )
             .long("audit")
-            .short("A"),
+            .short('A'),
     );
 
     #[cfg(any(target_os = "openbsd"))]
     let app = app.arg(
-        Arg::with_name("rtable")
+        Arg::new("rtable")
             .help("Display the routing table of the current process.")
             .long("rtable")
-            .short("R"),
+            .short('R'),
     );
 
     app
