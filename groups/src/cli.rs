073921575b75b4c0b600e14d816888aabd47219c
GrayJack <gr41.j4ck@gmail.com>
2022-01-02 05:50:28 -0300

    Groups: Update to clap 3.0.0

--- groups/src/cli.rs
+++ groups/src/cli.rs
@@ -1,25 +1,16 @@
-use clap::{
-    crate_authors, crate_description, crate_name, crate_version, App, AppSettings::ColoredHelp, Arg,
-};
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
 
-pub(crate) fn create_app<'a, 'b>() -> App<'a, 'b> {
+pub(crate) fn create_app<'help>() -> App<'help> {
     App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
-        .help_message("Display help information.")
-        .version_message("Display version information.")
-        .help_short("?")
-        .settings(&[ColoredHelp])
+        .mut_arg("help", |help| help.help("Display help information.").short('?'))
+        .mut_arg("version", |v| v.help("Display version information."))
         .arg(
-            Arg::with_name("USER")
+            Arg::new("USER")
                 .help("Name of a user.")
                 .long_help("Name of a user.\n\nIf not specified assumes the current user"),
         )
-        .arg(
-            Arg::with_name("id")
-                .help("Give the group id with the group name.")
-                .long("id")
-                .short("i"),
-        )
+        .arg(Arg::new("id").help("Give the group id with the group name.").long("id").short('i'))
 }
