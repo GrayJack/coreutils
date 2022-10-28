d1e1af3fee9b4f36923481ddadf0d502925e3d4e
ShisuiMadara <2020ucs0090@iitjammu.ac.in>
2022-10-28 16:44:22 +0530

    fixed comm and initialized join files

---
+++ join/src/cli.rs
@@ -0,0 +1,11 @@
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
+
+pub(crate) fn create_app<'help>() -> App<'help> {
+    App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .mut_arg("help", |help| help.help("Display help information.").short('?'))
+        .mut_arg("version", |v| v.help("Display version information."))
+       
+}
